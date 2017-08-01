ee8d7b1555b429b191cabe35a5575191948828b3
baba-beda <darya.nightmare@gmail.com>
2017-08-01 12:38:46 +0300

    added about page ctlab/phantasus#43

--- js/phantasus.js
+++ js/phantasus.js
@@ -17135,7 +17135,7 @@ phantasus.PcaPlotTool.prototype = {
           $chart.appendTo(_this.$chart);
           //// console.log(data, layout, config);
 
-          layout.showlegend = !(size == 12 && color == null);
+          layout.showlegend = !(size === 12 && color == null);
           Plotly.newPlot(myPlot, data, layout, config);
         };
 
@@ -32553,17 +32553,20 @@ phantasus.HelpMenu = function () {
     ' dropdown-toggle"' +
     ' data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">');
   html.push('<svg width="14px" height="14px" style="inline-block;vertical-align:middle;"><g><rect x="0" y="0" width="14" height="6" style="fill:#ca0020;stroke:none"></rect><rect x="0" y="7" width="14" height="6" style="fill:#0571b0;stroke:none"></rect></g></svg>');
-  html.push(' <span class="fa fa-caret-down"></span>');
+  html.push('<span class="fa fa-caret-down"></span>');
   html.push('</button>');
   html
     .push('<ul class="dropdown-menu dropdown-menu-right" role="menu">');
-  html.push('<li><a data-name="contact" href="#">Contact</a></li>');
 
-  html.push('<li><a data-name="linking" href="#">Linking</a></li>');
-  html.push('<li><a data-name="tutorial" href="#">Tutorial</a></li>');
-  html.push('<li><a data-name="source" href="#">Source Code</a></li>');
-  html.push('<li role="presentation" class="divider"></li>');
-  html.push('<li><a data-name="keymap" href="#">Keymap Reference</a></li>');
+  html.push('<li><a data-name="about" href="about.html">About</a></li>');
+
+  // html.push('<li><a data-name="contact" href="#">Contact</a></li>');
+
+  // html.push('<li><a data-name="linking" href="#">Linking</a></li>');
+  // html.push('<li><a data-name="tutorial" href="tutorial.html">Tutorial</a></li>');
+  // html.push('<li><a data-name="source" href="#">Source Code</a></li>');
+  // html.push('<li role="presentation" class="divider"></li>');
+  // html.push('<li><a data-name="keymap" href="#">Keymap Reference</a></li>');
 
   html.push('</ul>');
   html.push('</div>');
