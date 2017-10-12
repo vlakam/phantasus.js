cmap/morpheus.js

preserve focus with modals

src/matrix/vector_keys.js
@@ -19,8 +19,11 @@ morpheus.VectorKeys.FUNCTION = 'morpheus.funct';
 /** Indicates that vector values are dynamically computed based on selection */
 morpheus.VectorKeys.SELECTION = 'morpheus.selection';
 
-/** Whether to recompute a function when creating a new heat map */
-morpheus.VectorKeys.RECOMPUTE_FUNCTION = 'morpheus.recompute.funct';
+/** Whether to recompute a function when creating a new heat map (true or false) */
+morpheus.VectorKeys.RECOMPUTE_FUNCTION_NEW_HEAT_MAP = 'morpheus.recompute.funct.new.heat.map';
+
+/** Whether to recompute a function when filter is updated (true or false)  */
+morpheus.VectorKeys.RECOMPUTE_FUNCTION_FILTER = 'morpheus.recompute.funct.filter';
 
 /** Boolean, whether to recompute a function when heat map selection changes */
 morpheus.VectorKeys.RECOMPUTE_FUNCTION_SELECTION = 'morpheus.recompute.funct.selection';

src/ui/heat_map.js
@@ -278,6 +278,7 @@ morpheus.HeatMap = function (options) {
     morpheus.FormBuilder.showOkCancel({
       title: 'Dataset',
       appendTo: this.getContentEl(),
+      focus: this.getFocusEl(),
       content: datasetFormBuilder.$form,
       okCallback: function () {
         var file = datasetFormBuilder.getValue('file');
@@ -459,7 +460,8 @@ morpheus.HeatMap = function (options) {
       morpheus.FormBuilder.showInModal({
         title: 'Error',
         html: message,
-        appendTo: _this.getContentEl()
+        appendTo: _this.getContentEl(),
+        focus: _this.getFocusEl()
       });
     });
     d
@@ -539,7 +541,8 @@ morpheus.HeatMap = function (options) {
       morpheus.FormBuilder.showInModal({
         title: 'Error',
         html: message.join(''),
-        appendTo: _this.getContentEl()
+        appendTo: _this.getContentEl(),
+        focus: _this.getFocusEl()
       });
     });
 
@@ -1918,6 +1921,14 @@ morpheus.HeatMap.prototype = {
     .on(
       'rowFilterChanged columnFilterChanged rowGroupByChanged columnGroupByChanged rowSortOrderChanged columnSortOrderChanged datasetChanged',
       function (e) {
+        if (e.type === 'datasetChanged' || e.type === 'columnFilterChanged') {
+          var dataset = _this.project.getFullDataset();
+          morpheus.Project._recomputeCalculatedColumnFields(new morpheus.TransposedDatasetView(dataset), morpheus.VectorKeys.RECOMPUTE_FUNCTION_FILTER);
+        }
+        if (e.type === 'datasetChanged' || e.type === 'rowFilterChanged') {
+          var dataset = _this.project.getFullDataset();
+          morpheus.Project._recomputeCalculatedColumnFields(dataset, morpheus.VectorKeys.RECOMPUTE_FUNCTION_FILTER);
+        }
         if (e.type === 'datasetChanged') { // remove
           // tracks
           // that are no
@@ -3275,12 +3286,14 @@ morpheus.HeatMap.prototype = {
       // saveAs(new Blob([s.join('')], {
       // 	type: 'text/plain;charset=utf-8'
       // }), 'canvas.txt', true);
-      canvas.toBlob(function (blob) {
+      var toBlob = canvas.toBlobHD ? ['toBlobHD'] : 'toBlob';
+      canvas[toBlob](function (blob) {
         if (blob == null || blob.size === 0) {
           morpheus.FormBuilder.showInModal({
             title: 'Save Image',
             html: 'Image is too large to save.',
-            appendTo: _this.getContentEl()
+            appendTo: _this.getContentEl(),
+            focus: _this.getFocusEl()
           });
           return;
         }

vlakam/phantasus.js

src/matrix/vector_keys.js
@@ -19,8 +19,11 @@ phantasus.VectorKeys.FUNCTION = 'phantasus.funct';
 /** Indicates that vector values are dynamically computed based on selection */
 phantasus.VectorKeys.SELECTION = 'phantasus.selection';
 
-/** Whether to recompute a function when creating a new heat map */
-phantasus.VectorKeys.RECOMPUTE_FUNCTION = 'phantasus.recompute.funct';
+/** Whether to recompute a function when creating a new heat map (true or false) */
+phantasus.VectorKeys.RECOMPUTE_FUNCTION_NEW_HEAT_MAP = 'phantasus.recompute.funct.new.heat.map';
+
+/** Whether to recompute a function when filter is updated (true or false)  */
+phantasus.VectorKeys.RECOMPUTE_FUNCTION_FILTER = 'phantasus.recompute.funct.filter';
 
 /** Boolean, whether to recompute a function when heat map selection changes */
 phantasus.VectorKeys.RECOMPUTE_FUNCTION_SELECTION = 'phantasus.recompute.funct.selection';

src/ui/heat_map.js
@@ -283,6 +283,7 @@ phantasus.HeatMap = function (options) {
     phantasus.FormBuilder.showOkCancel({
       title: 'Dataset',
       appendTo: this.getContentEl(),
+      focus: this.getFocusEl(),
       content: datasetFormBuilder.$form,
       okCallback: function () {
         var file = datasetFormBuilder.getValue('file');
@@ -486,7 +487,8 @@ phantasus.HeatMap = function (options) {
       phantasus.FormBuilder.showInModal({
         title: 'Error',
         html: message,
-        appendTo: _this.getContentEl()
+        appendTo: _this.getContentEl(),
+        focus: _this.getFocusEl()
       });
     });
     d
@@ -585,7 +587,8 @@ phantasus.HeatMap = function (options) {
       phantasus.FormBuilder.showInModal({
         title: 'Error',
         html: message.join(''),
-        appendTo: _this.getContentEl()
+        appendTo: _this.getContentEl(),
+        focus: _this.getFocusEl()
       });
     });
 
@@ -1966,7 +1969,15 @@ phantasus.HeatMap.prototype = {
       .on(
         'rowFilterChanged columnFilterChanged rowGroupByChanged columnGroupByChanged rowSortOrderChanged columnSortOrderChanged datasetChanged',
         function (e) {
-          if (e.type === 'datasetChanged') { // remove
+          if (e.type === 'datasetChanged' || e.type === 'columnFilterChanged') {
+          var dataset = _this.project.getFullDataset();
+          phantasus.Project._recomputeCalculatedColumnFields(new phantasus.TransposedDatasetView(dataset), phantasus.VectorKeys.RECOMPUTE_FUNCTION_FILTER);
+        }
+        if (e.type === 'datasetChanged' || e.type === 'rowFilterChanged') {
+          var dataset = _this.project.getFullDataset();
+          phantasus.Project._recomputeCalculatedColumnFields(dataset, phantasus.VectorKeys.RECOMPUTE_FUNCTION_FILTER);
+        }
+        if (e.type === 'datasetChanged') { // remove
             // tracks
             // that are no
             // longer in the
@@ -3323,12 +3334,14 @@ phantasus.HeatMap.prototype = {
       // saveAs(new Blob([s.join('')], {
       // 	type: 'text/plain;charset=utf-8'
       // }), 'canvas.txt', true);
-      canvas.toBlob(function (blob) {
+      var toBlob = canvas.toBlobHD ? ['toBlobHD'] : 'toBlob';
+      canvas[toBlob](function (blob) {
         if (blob == null || blob.size === 0) {
           phantasus.FormBuilder.showInModal({
             title: 'Save Image',
             html: 'Image is too large to save.',
-            appendTo: _this.getContentEl()
+            appendTo: _this.getContentEl(),
+            focus: _this.getFocusEl()
           });
           return;
         }