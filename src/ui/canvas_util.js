cmap/morpheus.js

header background

src/ui/canvas_util.js
@@ -3,7 +3,7 @@ morpheus.CanvasUtil = function () {
 morpheus.CanvasUtil.dragging = false;
 
 morpheus.CanvasUtil.FONT_NAME = '"Helvetica Neue",Helvetica,Arial,sans-serif';
-morpheus.CanvasUtil.FONT_COLOR = 'rgb(34, 34, 34)';
+morpheus.CanvasUtil.FONT_COLOR = 'rgb(0, 0, 0)';
 morpheus.CanvasUtil.getPreferredSize = function (c) {
   var size = c.getPreferredSize();
   var prefWidth = c.getPrefWidth();

vlakam/phantasus.js

src/ui/canvas_util.js
@@ -3,7 +3,7 @@ phantasus.CanvasUtil = function () {
 phantasus.CanvasUtil.dragging = false;
 
 phantasus.CanvasUtil.FONT_NAME = '"Helvetica Neue",Helvetica,Arial,sans-serif';
-phantasus.CanvasUtil.FONT_COLOR = 'rgb(34, 34, 34)';
+phantasus.CanvasUtil.FONT_COLOR = 'rgb(0, 0, 0)';
 phantasus.CanvasUtil.getPreferredSize = function (c) {
   var size = c.getPreferredSize();
   var prefWidth = c.getPrefWidth();