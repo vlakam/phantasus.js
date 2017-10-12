cmap/morpheus.js

lock sort keys. Fixed check for continuous color schemes

src/matrix/sort_key.js
@@ -22,6 +22,7 @@ morpheus.MatchesOnTopSortKey = function (project, modelIndices, name, columns) {
   this.columns = columns;
 };
 morpheus.MatchesOnTopSortKey.prototype = {
+  lockOrder: 0,
   /**
    * Indicates whether this key is sorting rows or columns.
    * @return {*}
@@ -45,6 +46,12 @@ morpheus.MatchesOnTopSortKey.prototype = {
   },
   toString: function (i) {
     return this.name;
+  },
+  getLockOrder: function () {
+    return this.lockOrder;
+  },
+  setLockOrder: function (lockOrder) {
+    this.lockOrder = lockOrder;
   }
 };
 morpheus.SortKey = function (field, sortOrder, columns) {
@@ -60,6 +67,7 @@ morpheus.SortKey = function (field, sortOrder, columns) {
 };
 
 morpheus.SortKey.prototype = {
+  lockOrder: 0,
   isColumns: function () {
     return this.columns;
   },
@@ -173,6 +181,12 @@ morpheus.SortKey.prototype = {
   ,
   toString: function () {
     return this.field;
+  },
+  getLockOrder: function () {
+    return this.lockOrder;
+  },
+  setLockOrder: function (lockOrder) {
+    this.lockOrder = lockOrder;
   }
 }
 ;
@@ -192,6 +206,7 @@ morpheus.SortByValuesKey = function (modelIndices, sortOrder, isColumnSort) {
 
 };
 morpheus.SortByValuesKey.prototype = {
+  lockOrder: 0,
   isColumns: function () {
     return this.isColumnSort;
   },
@@ -294,6 +309,12 @@ morpheus.SortByValuesKey.prototype = {
   },
   toString: function () {
     return 'values';
+  },
+  getLockOrder: function () {
+    return this.lockOrder;
+  },
+  setLockOrder: function (lockOrder) {
+    this.lockOrder = lockOrder;
   }
 };
 /**
@@ -320,6 +341,7 @@ morpheus.SpecifiedModelSortOrder = function (modelIndices, nvisible, name, colum
   this.columns = columns;
 };
 morpheus.SpecifiedModelSortOrder.prototype = {
+  lockOrder: 0,
   isColumns: function () {
     return this.columns;
   },
@@ -344,6 +366,12 @@ morpheus.SpecifiedModelSortOrder.prototype = {
   },
   getName: function () {
     return this.name;
+  },
+  getLockOrder: function () {
+    return this.lockOrder;
+  },
+  setLockOrder: function (lockOrder) {
+    this.lockOrder = lockOrder;
   }
 };
 
@@ -653,12 +681,10 @@ morpheus.SortKey.keepExistingSortKeys = function (newSortKeys, existingSortKeys)
       && key.name === 'dendrogram') {
       dendrogramSortKey = key;
     }
-  }
-  if (matchesOnTopSortKey) {
-    newSortKeys.splice(0, 0, matchesOnTopSortKey);
-  }
-  if (dendrogramSortKey) {
-    newSortKeys.splice(newSortKeys.length, 0, dendrogramSortKey);
+    if (key.getLockOrder() > 0) {
+      // 1 is beginning, 2 is end
+      newSortKeys.splice(key.getLockOrder() === 1 ? 0 : newSortKeys.length, 0, key);
+    }
   }
   return newSortKeys;
 };
@@ -666,20 +692,27 @@ morpheus.SortKey.keepExistingSortKeys = function (newSortKeys, existingSortKeys)
 morpheus.SortKey.fromJSON = function (project, json) {
   var sortKeys = [];
   json.forEach(function (key) {
+    var sortKey = null;
     if (key.type === 'annotation') {
-      sortKeys.push(new morpheus.SortKey(key.field, key.order, key.isColumns));
+      sortKey = new morpheus.SortKey(key.field, key.order, key.isColumns);
     } else if (key.type === 'byValues') {
-      sortKeys.push(new morpheus.SortByValuesKey(key.modelIndices, key.order, key.isColumns));
+      sortKey = new morpheus.SortByValuesKey(key.modelIndices, key.order, key.isColumns);
     } else if (key.type === 'specified') {
-      sortKeys.push(new morpheus.SpecifiedModelSortOrder(key.modelIndices, key.nvisible, key.name, key.isColumns));
+      sortKey = new morpheus.SpecifiedModelSortOrder(key.modelIndices, key.nvisible, key.name, key.isColumns);
     } else if (key.type === 'matchesOnTop') {
-      sortKeys.push(new morpheus.MatchesOnTopSortKey(project, key.modelIndices, key.name, key.isColumns));
+      sortKey = new morpheus.MatchesOnTopSortKey(project, key.modelIndices, key.name, key.isColumns);
     } else {
       if (key.field != null) {
-        sortKeys.push(new morpheus.SortKey(key.field, key.order));
+        sortKey = new morpheus.SortKey(key.field, key.order);
       } else {
         console.log('Unknown key: ' + key);
       }
+    }
+    if (sortKey != null) {
+      if (key.lockOrder !== 0) {
+        sortKey.setLockOrder(key.lockOrder);
+      }
+      sortKeys.push(sortKey);
     }
   });
   return sortKeys;
@@ -688,37 +721,44 @@ morpheus.SortKey.fromJSON = function (project, json) {
 morpheus.SortKey.toJSON = function (sortKeys) {
   var json = [];
   sortKeys.forEach(function (key) {
+    var sortKey = null;
     if (key instanceof morpheus.SortKey) {
-      json.push({
+      sortKey = {
         isColumns: key.isColumns(),
         order: key.getSortOrder(),
         type: 'annotation',
         field: '' + key,
-      });
+      };
     } else if (key instanceof morpheus.SortByValuesKey) {
-      json.push({
+      sortKey = {
         isColumns: key.isColumns(),
         order: key.getSortOrder(),
         type: 'byValues',
         modelIndices: key.modelIndices
-      });
+      };
     } else if (key instanceof morpheus.SpecifiedModelSortOrder) {
-      json.push({
+      sortKey = {
         isColumns: key.isColumns(),
         order: key.getSortOrder(),
         type: 'specified',
         modelIndices: key.modelIndices,
         name: key.name,
         nvisible: key.nvisible
-      });
+      };
     } else if (key instanceof morpheus.MatchesOnTopSortKey) {
-      json.push({
+      sortKey = {
         isColumns: key.isColumns(),
         order: key.getSortOrder(),
         type: 'matchesOnTop',
         modelIndices: key.modelIndices,
         name: key.name
-      });
+      };
+    }
+    if (sortKey != null) {
+      if (key.getLockOrder && key.getLockOrder() !== 0) {
+        sortKey.lockOrder = key.getLockOrder();
+      }
+      json.push(sortKey);
     } else {
       console.log('Unknown sort key type');
     }

src/ui/heat_map.js
@@ -1100,17 +1100,21 @@ morpheus.HeatMap.prototype = {
         dendrogram.filter = this.project.getColumnFilter()
         .shallowClone();
         this.columnDendrogram = dendrogram;
+        var sortKey = new morpheus.SpecifiedModelSortOrder(modelOrder,
+          modelOrder.length, 'dendrogram', true);
+        sortKey.setLockOrder(2);
         this.project.setColumnSortKeys(
-          [new morpheus.SpecifiedModelSortOrder(modelOrder,
-            modelOrder.length, 'dendrogram', true)], true);
+          [sortKey], true);
       } else {
         dendrogram = new morpheus.RowDendrogram(this, tree,
           this.heatmap.getRowPositions(), this.project);
         dendrogram.filter = this.project.getRowFilter().shallowClone();
         this.rowDendrogram = dendrogram;
+        var sortKey = new morpheus.SpecifiedModelSortOrder(modelOrder,
+          modelOrder.length, 'dendrogram', false);
+        sortKey.setLockOrder(2);
         this.project.setRowSortKeys(
-          [new morpheus.SpecifiedModelSortOrder(modelOrder,
-            modelOrder.length, 'dendrogram', false)], true);
+          [sortKey], true);
       }
       dendrogram.appendTo(this.$parent);
       dendrogram.$label.appendTo(this.$parent);
@@ -1437,6 +1441,7 @@ morpheus.HeatMap.prototype = {
       if (rowIndices != null) {
         rowDendrogramSortKey = new morpheus.SpecifiedModelSortOrder(
           rowIndices, rowIndices.length, 'dendrogram');
+        rowDendrogramSortKey.setLockOrder(2);
       }
     }
     var columnDendrogramSortKey = null;
@@ -1494,6 +1499,7 @@ morpheus.HeatMap.prototype = {
       if (columnIndices != null) {
         columnDendrogramSortKey = new morpheus.SpecifiedModelSortOrder(
           columnIndices, columnIndices.length, 'dendrogram');
+        columnDendrogramSortKey.setLockOrder(2);
       }
     }
 
@@ -3794,15 +3800,35 @@ morpheus.HeatMap.prototype = {
     for (var i = 0, length = this.rowTracks.length; i < length; i++) {
       if (this.rowTracks[i].isVisible()) {
         rowTrackWidthSum += Math
-        .max(
-          morpheus.CanvasUtil
+        .max(morpheus.CanvasUtil
           .getPreferredSize(this.rowTrackHeaders[i]).width,
           morpheus.CanvasUtil
           .getPreferredSize(this.rowTracks[i]).width);
       }
     }
+    if ((rowTrackWidthSum + rowDendrogramWidth + heatmapPrefSize.width) > availableWidth) {
+      // shrink row tracks
+      //var over = (rowTrackWidthSum + rowDendrogramWidth + heatmapPrefSize.width) - availableWidth;
+      rowTrackWidthSum = 0;
+      for (var i = 0, length = this.rowTracks.length; i < length; i++) {
+        if (this.rowTracks[i].isVisible()) {
+          var rowTrackHeaderSize = morpheus.CanvasUtil
+          .getPreferredSize(this.rowTrackHeaders[i]);
+          if (!rowTrackHeaderSize.widthSet) {
+            var width = Math.max(rowTrackHeaderSize.width, morpheus.CanvasUtil
+            .getPreferredSize(this.rowTracks[i]).width);
+            width = Math.min(400, width);
+            this.rowTracks[i].setPrefWidth(width);
+            this.rowTrackHeaders[i].setPrefWidth(width);
+            rowTrackWidthSum += width;
+          }
+        }
+      }
+    }
+
     var ypos = columnDendrogramHeight;
     var maxHeaderWidth = 0;
+    // get max column header width
     for (var i = 0, length = this.columnTracks.length; i < length; i++) {
       if (this.columnTracks[i].isVisible()) {
         var width = morpheus.CanvasUtil
@@ -3952,6 +3978,7 @@ morpheus.HeatMap.prototype = {
       // afterVerticalScrollBarDivider
     }
     var rowAnnotationXStart = xpos;
+    // set row track bounds
     for (var i = 0, length = this.rowTracks.length; i < length; i++) {
       var track = this.rowTracks[i];
       if (track.isVisible()) {

vlakam/phantasus.js

src/matrix/sort_key.js
@@ -22,6 +22,7 @@ phantasus.MatchesOnTopSortKey = function (project, modelIndices, name, columns) 
   this.columns = columns;
 };
 phantasus.MatchesOnTopSortKey.prototype = {
+  lockOrder: 0,
   /**
    * Indicates whether this key is sorting rows or columns.
    * @return {*}
@@ -45,6 +46,12 @@ phantasus.MatchesOnTopSortKey.prototype = {
   },
   toString: function (i) {
     return this.name;
+  },
+  getLockOrder: function () {
+    return this.lockOrder;
+  },
+  setLockOrder: function (lockOrder) {
+    this.lockOrder = lockOrder;
   }
 };
 phantasus.SortKey = function (field, sortOrder, columns) {
@@ -60,6 +67,7 @@ phantasus.SortKey = function (field, sortOrder, columns) {
 };
 
 phantasus.SortKey.prototype = {
+  lockOrder: 0,
   isColumns: function () {
     return this.columns;
   },
@@ -173,6 +181,12 @@ phantasus.SortKey.prototype = {
   ,
   toString: function () {
     return this.field;
+  },
+  getLockOrder: function () {
+    return this.lockOrder;
+  },
+  setLockOrder: function (lockOrder) {
+    this.lockOrder = lockOrder;
   }
 }
 ;
@@ -192,6 +206,7 @@ phantasus.SortByValuesKey = function (modelIndices, sortOrder, isColumnSort) {
 
 };
 phantasus.SortByValuesKey.prototype = {
+  lockOrder: 0,
   isColumns: function () {
     return this.isColumnSort;
   },
@@ -294,6 +309,12 @@ phantasus.SortByValuesKey.prototype = {
   },
   toString: function () {
     return 'values';
+  },
+  getLockOrder: function () {
+    return this.lockOrder;
+  },
+  setLockOrder: function (lockOrder) {
+    this.lockOrder = lockOrder;
   }
 };
 /**
@@ -320,6 +341,7 @@ phantasus.SpecifiedModelSortOrder = function (modelIndices, nvisible, name, colu
   this.columns = columns;
 };
 phantasus.SpecifiedModelSortOrder.prototype = {
+  lockOrder: 0,
   isColumns: function () {
     return this.columns;
   },
@@ -344,6 +366,12 @@ phantasus.SpecifiedModelSortOrder.prototype = {
   },
   getName: function () {
     return this.name;
+  },
+  getLockOrder: function () {
+    return this.lockOrder;
+  },
+  setLockOrder: function (lockOrder) {
+    this.lockOrder = lockOrder;
   }
 };
 
@@ -653,12 +681,10 @@ phantasus.SortKey.keepExistingSortKeys = function (newSortKeys, existingSortKeys
       && key.name === 'dendrogram') {
       dendrogramSortKey = key;
     }
-  }
-  if (matchesOnTopSortKey) {
-    newSortKeys.splice(0, 0, matchesOnTopSortKey);
-  }
-  if (dendrogramSortKey) {
-    newSortKeys.splice(newSortKeys.length, 0, dendrogramSortKey);
+    if (key.getLockOrder() > 0) {
+      // 1 is beginning, 2 is end
+      newSortKeys.splice(key.getLockOrder() === 1 ? 0 : newSortKeys.length, 0, key);
+    }
   }
   return newSortKeys;
 };
@@ -666,20 +692,27 @@ phantasus.SortKey.keepExistingSortKeys = function (newSortKeys, existingSortKeys
 phantasus.SortKey.fromJSON = function (project, json) {
   var sortKeys = [];
   json.forEach(function (key) {
+    var sortKey = null;
     if (key.type === 'annotation') {
-      sortKeys.push(new phantasus.SortKey(key.field, key.order, key.isColumns));
+      sortKey = new phantasus.SortKey(key.field, key.order, key.isColumns);
     } else if (key.type === 'byValues') {
-      sortKeys.push(new phantasus.SortByValuesKey(key.modelIndices, key.order, key.isColumns));
+      sortKey = new phantasus.SortByValuesKey(key.modelIndices, key.order, key.isColumns);
     } else if (key.type === 'specified') {
-      sortKeys.push(new phantasus.SpecifiedModelSortOrder(key.modelIndices, key.nvisible, key.name, key.isColumns));
+      sortKey = new phantasus.SpecifiedModelSortOrder(key.modelIndices, key.nvisible, key.name, key.isColumns);
     } else if (key.type === 'matchesOnTop') {
-      sortKeys.push(new phantasus.MatchesOnTopSortKey(project, key.modelIndices, key.name, key.isColumns));
+      sortKey = new phantasus.MatchesOnTopSortKey(project, key.modelIndices, key.name, key.isColumns);
     } else {
       if (key.field != null) {
-        sortKeys.push(new phantasus.SortKey(key.field, key.order));
+        sortKey = new phantasus.SortKey(key.field, key.order);
       } else {
         // console.log('Unknown key: ' + key);
       }
+    }
+    if (sortKey != null) {
+      if (key.lockOrder !== 0) {
+        sortKey.setLockOrder(key.lockOrder);
+      }
+      sortKeys.push(sortKey);
     }
   });
   return sortKeys;
@@ -688,37 +721,44 @@ phantasus.SortKey.fromJSON = function (project, json) {
 phantasus.SortKey.toJSON = function (sortKeys) {
   var json = [];
   sortKeys.forEach(function (key) {
+    var sortKey = null;
     if (key instanceof phantasus.SortKey) {
-      json.push({
+      sortKey = {
         isColumns: key.isColumns(),
         order: key.getSortOrder(),
         type: 'annotation',
         field: '' + key,
-      });
+      };
     } else if (key instanceof phantasus.SortByValuesKey) {
-      json.push({
+      sortKey = {
         isColumns: key.isColumns(),
         order: key.getSortOrder(),
         type: 'byValues',
         modelIndices: key.modelIndices
-      });
+      };
     } else if (key instanceof phantasus.SpecifiedModelSortOrder) {
-      json.push({
+      sortKey = {
         isColumns: key.isColumns(),
         order: key.getSortOrder(),
         type: 'specified',
         modelIndices: key.modelIndices,
         name: key.name,
         nvisible: key.nvisible
-      });
+      };
     } else if (key instanceof phantasus.MatchesOnTopSortKey) {
-      json.push({
+      sortKey = {
         isColumns: key.isColumns(),
         order: key.getSortOrder(),
         type: 'matchesOnTop',
         modelIndices: key.modelIndices,
         name: key.name
-      });
+      };
+    }
+    if (sortKey != null) {
+      if (key.getLockOrder && key.getLockOrder() !== 0) {
+        sortKey.lockOrder = key.getLockOrder();
+      }
+      json.push(sortKey);
     } else {
       // console.log('Unknown sort key type');
     }

src/ui/heat_map.js
@@ -1145,17 +1145,21 @@ phantasus.HeatMap.prototype = {
         dendrogram.filter = this.project.getColumnFilter()
           .shallowClone();
         this.columnDendrogram = dendrogram;
+        var sortKey = new phantasus.SpecifiedModelSortOrder(modelOrder,
+          modelOrder.length, 'dendrogram', true);
+        sortKey.setLockOrder(2);
         this.project.setColumnSortKeys(
-          [new phantasus.SpecifiedModelSortOrder(modelOrder,
-            modelOrder.length, 'dendrogram', true)], true);
+          [sortKey], true);
       } else {
         dendrogram = new phantasus.RowDendrogram(this, tree,
           this.heatmap.getRowPositions(), this.project);
         dendrogram.filter = this.project.getRowFilter().shallowClone();
         this.rowDendrogram = dendrogram;
+        var sortKey = new phantasus.SpecifiedModelSortOrder(modelOrder,
+          modelOrder.length, 'dendrogram', false);
+        sortKey.setLockOrder(2);
         this.project.setRowSortKeys(
-          [new phantasus.SpecifiedModelSortOrder(modelOrder,
-            modelOrder.length, 'dendrogram', false)], true);
+          [sortKey], true);
       }
       dendrogram.appendTo(this.$parent);
       dendrogram.$label.appendTo(this.$parent);
@@ -1482,6 +1486,7 @@ phantasus.HeatMap.prototype = {
       if (rowIndices != null) {
         rowDendrogramSortKey = new phantasus.SpecifiedModelSortOrder(
           rowIndices, rowIndices.length, 'dendrogram');
+        rowDendrogramSortKey.setLockOrder(2);
       }
     }
     var columnDendrogramSortKey = null;
@@ -1539,6 +1544,7 @@ phantasus.HeatMap.prototype = {
       if (columnIndices != null) {
         columnDendrogramSortKey = new phantasus.SpecifiedModelSortOrder(
           columnIndices, columnIndices.length, 'dendrogram');
+        columnDendrogramSortKey.setLockOrder(2);
       }
     }
 
@@ -3840,15 +3846,35 @@ phantasus.HeatMap.prototype = {
     for (var i = 0, length = this.rowTracks.length; i < length; i++) {
       if (this.rowTracks[i].isVisible()) {
         rowTrackWidthSum += Math
-          .max(
+          .max(phantasus.CanvasUtil
+            .getPreferredSize(this.rowTrackHeaders[i]).width,
             phantasus.CanvasUtil
-              .getPreferredSize(this.rowTrackHeaders[i]).width,
-            phantasus.CanvasUtil
-              .getPreferredSize(this.rowTracks[i]).width);
+            .getPreferredSize(this.rowTracks[i]).width);
       }
     }
+    if ((rowTrackWidthSum + rowDendrogramWidth + heatmapPrefSize.width) > availableWidth) {
+      // shrink row tracks
+      //var over = (rowTrackWidthSum + rowDendrogramWidth + heatmapPrefSize.width) - availableWidth;
+      rowTrackWidthSum = 0;
+      for (var i = 0, length = this.rowTracks.length; i < length; i++) {
+        if (this.rowTracks[i].isVisible()) {
+          var rowTrackHeaderSize = phantasus.CanvasUtil
+          .getPreferredSize(this.rowTrackHeaders[i]);
+          if (!rowTrackHeaderSize.widthSet) {
+            var width = Math.max(rowTrackHeaderSize.width, phantasus.CanvasUtil
+            .getPreferredSize(this.rowTracks[i]).width);
+            width = Math.min(400, width);
+            this.rowTracks[i].setPrefWidth(width);
+            this.rowTrackHeaders[i].setPrefWidth(width);
+            rowTrackWidthSum += width;
+          }
+        }
+      }
+    }
+
     var ypos = columnDendrogramHeight;
     var maxHeaderWidth = 0;
+    // get max column header width
     for (var i = 0, length = this.columnTracks.length; i < length; i++) {
       if (this.columnTracks[i].isVisible()) {
         var width = phantasus.CanvasUtil
@@ -3998,6 +4024,7 @@ phantasus.HeatMap.prototype = {
       // afterVerticalScrollBarDivider
     }
     var rowAnnotationXStart = xpos;
+    // set row track bounds
     for (var i = 0, length = this.rowTracks.length; i < length; i++) {
       var track = this.rowTracks[i];
       if (track.isVisible()) {