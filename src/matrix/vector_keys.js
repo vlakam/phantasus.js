cmap/morpheus.js

save number format

src/matrix/vector_keys.js
@@ -35,5 +35,6 @@ morpheus.VectorKeys.COPY_IGNORE.add(morpheus.VectorKeys.VALUE_TO_INDICES);
 
 morpheus.VectorKeys.JSON_WHITELIST = new morpheus.Set();
 morpheus.VectorKeys.JSON_WHITELIST.add(morpheus.VectorKeys.FIELDS);
+morpheus.VectorKeys.JSON_WHITELIST.add(morpheus.VectorKeys.FORMATTER);
 morpheus.VectorKeys.JSON_WHITELIST.add(morpheus.VectorKeys.DATA_TYPE);
 morpheus.VectorKeys.JSON_WHITELIST.add(morpheus.VectorKeys.TITLE);

src/ui/canvas_util.js
@@ -162,6 +162,10 @@ morpheus.CanvasUtil.getHeaderStringWidth = function (context, s) {
   context.font = '14px ' + morpheus.CanvasUtil.FONT_NAME;
   return context.measureText(s).width + 18;
 };
+
+morpheus.CanvasUtil.forceSubPixelRendering = function (context) {
+  context.getImageData(0, 0, 1, 1);
+};
 morpheus.CanvasUtil.getVectorStringWidth = function (context, vector, positions,
                                                      end) {
   if (positions.getSize() < 6) {

vlakam/phantasus.js

src/matrix/vector_keys.js
@@ -38,5 +38,6 @@ phantasus.VectorKeys.COPY_IGNORE.add(phantasus.VectorKeys.VALUE_TO_INDICES);
 
 phantasus.VectorKeys.JSON_WHITELIST = new phantasus.Set();
 phantasus.VectorKeys.JSON_WHITELIST.add(phantasus.VectorKeys.FIELDS);
+phantasus.VectorKeys.JSON_WHITELIST.add(phantasus.VectorKeys.FORMATTER);
 phantasus.VectorKeys.JSON_WHITELIST.add(phantasus.VectorKeys.DATA_TYPE);
 phantasus.VectorKeys.JSON_WHITELIST.add(phantasus.VectorKeys.TITLE);

src/ui/canvas_util.js
@@ -162,6 +162,10 @@ phantasus.CanvasUtil.getHeaderStringWidth = function (context, s) {
   context.font = '14px ' + phantasus.CanvasUtil.FONT_NAME;
   return context.measureText(s).width + 18;
 };
+
+phantasus.CanvasUtil.forceSubPixelRendering = function (context) {
+  context.getImageData(0, 0, 1, 1);
+};
 phantasus.CanvasUtil.getVectorStringWidth = function (context, vector, positions,
                                                      end) {
   if (positions.getSize() < 6) {