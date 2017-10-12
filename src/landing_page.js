cmap/morpheus.js

doc update. Marker selection tests.

src/landing_page.js
@@ -8,20 +8,8 @@ morpheus.LandingPage = function (pageOptions) {
   var $el = $('<div class="container" style="display: none;"></div>');
   this.$el = $el;
   var html = [];
+  morpheus.Util.createMorpheusHeader().appendTo($el);
   html.push('<div data-name="help" class="pull-right"></div>');
-
-  html
-  .push('<div style="margin-bottom:10px;"><svg width="32px" height="32px"><g><rect x="0" y="0" width="32" height="14" style="fill:#ca0020;stroke:none"/><rect x="0" y="18" width="32" height="14" style="fill:#0571b0;stroke:none"/></g></svg> <div data-name="brand" style="display:inline-block; vertical-align: top;font-size:24px;font-family:sans-serif;">');
-  html.push('<span>M</span>');
-  html.push('<span>o</span>');
-  html.push('<span>r</span>');
-  html.push('<span>p</span>');
-  html.push('<span>h</span>');
-  html.push('<span>e</span>');
-  html.push('<span>u</span>');
-  html.push('<span>s</span>');
-  html.push('</span>');
-  html.push('</div>');
 
   html.push('<h4>Open your own file</h4>');
   html.push('<div data-name="formRow" class="center-block"></div>');
@@ -29,8 +17,7 @@ morpheus.LandingPage = function (pageOptions) {
     ' dataset</h4></div>');
   html.push('</div>');
   var $html = $(html.join(''));
-  var colorScale = d3.scale.linear().domain([0, 4, 7]).range(['#ca0020', '#999999', '#0571b0']).clamp(true);
-  var brands = $html.find('[data-name="brand"] > span');
+
   $html.appendTo($el);
   new morpheus.HelpMenu().$el.appendTo($el.find('[data-name=help]'));
   var formBuilder = new morpheus.FormBuilder();
@@ -46,15 +33,7 @@ morpheus.LandingPage = function (pageOptions) {
   formBuilder.$form.appendTo($el.find('[data-name=formRow]'));
   this.formBuilder = formBuilder;
   this.$sampleDatasetsEl = $el.find('[data-name=preloadedDataset]');
-  var index = 0;
-  var step = function () {
-    brands[index].style.color = colorScale(index);
-    index++;
-    if (index < brands.length) {
-      setTimeout(step, 200);
-    }
-  };
-  setTimeout(step, 300);
+
   this.tabManager = new morpheus.TabManager({landingPage: this});
   this.tabManager.on('change rename add remove', function (e) {
     var title = _this.tabManager.getTabText(_this.tabManager.getActiveTabId());

src/tools/marker_selection.js
@@ -122,7 +122,7 @@ morpheus.MarkerSelection.prototype = {
         name: 'number_of_markers',
         value: '100',
         type: 'text',
-        help: 'The initial number of markers to show in each direction. Click <button title="Filter (Ctrl+L)" type="button" class="btn btn-default btn-xs dropdown-toggle"><span class="fa fa-filter"></span></button> to change.'
+        help: 'The initial number of markers to show in each direction.'
       }, {
         name: 'permutations',
         value: '0',
@@ -132,10 +132,20 @@ morpheus.MarkerSelection.prototype = {
   execute: function (options) {
 
     var project = options.project;
-    // classA and classB are arrays of identifiers if run via user
+    // classA and classB are arrays of Identifiers if run via user
     // interface. If run via JSON, will be string arrays
     var classA = options.input.class_a;
+    var classB = options.input.class_b;
+    if (classA.length === 0 && classB.length === 0) {
+      throw 'No samples in class A and class B';
+    }
 
+    if (classA.length === 0) {
+      throw 'No samples in class A';
+    }
+    if (classB.length === 0) {
+      throw 'No samples in class B';
+    }
     for (var i = 0; i < classA.length; i++) {
       var val = classA[i];
       if (!(val instanceof morpheus.Identifier)) {
@@ -143,7 +153,7 @@ morpheus.MarkerSelection.prototype = {
           morpheus.Util.isArray(val) ? val : [val]);
       }
     }
-    var classB = options.input.class_b;
+
     for (var i = 0; i < classB.length; i++) {
       var val = classB[i];
       if (!(val instanceof morpheus.Identifier)) {
@@ -152,18 +162,16 @@ morpheus.MarkerSelection.prototype = {
       }
     }
     var npermutations = parseInt(options.input.permutations);
+    var dataset = project.getSortedFilteredDataset();
+
     var fieldNames = options.input.field;
     if (!morpheus.Util.isArray(fieldNames)) {
       fieldNames = [fieldNames];
     }
-    var dataset = project.getSortedFilteredDataset();
+
     var vectors = morpheus.MetadataUtil.getVectors(dataset
     .getColumnMetadata(), fieldNames);
-
     var idToIndices = morpheus.VectorUtil.createValuesToIndicesMap(vectors);
-    var f = morpheus.MarkerSelection.Functions
-    .fromString(options.input.metric);
-
     var aIndices = [];
     var bIndices = [];
     classA.forEach(function (id) {
@@ -181,16 +189,8 @@ morpheus.MarkerSelection.prototype = {
       bIndices = bIndices.concat(indices);
     });
 
-    if (aIndices.length === 0 && bIndices.length === 0) {
-      throw 'No samples in class A and class B';
-    }
-
-    if (aIndices.length === 0) {
-      throw 'No samples in class A';
-    }
-    if (bIndices.length === 0) {
-      throw 'No samples in class B';
-    }
+    var f = morpheus.MarkerSelection.Functions
+    .fromString(options.input.metric);
 
     var classASet = {};
     for (var i = 0; i < aIndices.length; i++) {
@@ -237,8 +237,20 @@ morpheus.MarkerSelection.prototype = {
     for (var i = 0; i < bIndices.length; i++) {
       comparisonVector.setValue(bIndices[i], 'B');
     }
-    function done() {
+    function done(result) {
+      var pvalueVector = dataset.getRowMetadata().add('p_value');
+      var fdrVector = dataset.getRowMetadata().add('FDR(BH)');
+      var kVector = dataset.getRowMetadata().add('k');
 
+      for (var i = 0, size = pvalueVector.size(); i < size; i++) {
+        pvalueVector.setValue(i, result.rowSpecificPValues[i]);
+        fdrVector.setValue(i, result.fdr[i]);
+        kVector.setValue(i, result.k[i]);
+        v.setValue(i, result.scores[i]);
+      }
+      vectors.push(pvalueVector);
+      vectors.push(fdrVector);
+      vectors.push(kVector);
       if (project.getRowFilter().getFilters().length > 0) {
         project.getRowFilter().setAnd(true, true);
       }
@@ -319,51 +331,42 @@ morpheus.MarkerSelection.prototype = {
       vectors.push(contingencyTableVector);
       done();
     } else {
-      // background
       if (npermutations > 0) {
-        options.input.numClassA = aIndices.length;
-        options.input.npermutations = npermutations;
-        var blob = new Blob(
-          ['self.onmessage = function(e) {'
-          + 'importScripts(e.data.scripts);'
-          + 'self.postMessage(morpheus.MarkerSelection.execute(morpheus.Dataset.fromJSON(e.data.dataset), e.data.input));'
-          + '}']);
-
-        var url = window.URL.createObjectURL(blob);
-        var worker = new Worker(url);
         var subset = new morpheus.SlicedDatasetView(dataset, null,
           aIndices.concat(bIndices));
 
-        worker.postMessage({
-          scripts: morpheus.Util.getScriptPath(),
-          dataset: morpheus.Dataset.toJSON(subset, {
-            columnFields: [],
-            rowFields: [],
-            seriesIndices: [0]
-          }),
-          input: options.input
-        });
+        options.input.background = options.input.background && typeof Worker !== 'undefined';
+        options.input.numClassA = aIndices.length;
+        options.input.npermutations = npermutations;
+        if (options.input.background) {
+          var blob = new Blob(
+            ['self.onmessage = function(e) {'
+            + 'importScripts(e.data.scripts);'
+            + 'self.postMessage(morpheus.MarkerSelection.execute(morpheus.Dataset.fromJSON(e.data.dataset), e.data.input));'
+            + '}']);
 
-        worker.onmessage = function (e) {
-          var result = e.data;
-          var pvalueVector = dataset.getRowMetadata().add('p_value');
-          var fdrVector = dataset.getRowMetadata().add('FDR(BH)');
-          var kVector = dataset.getRowMetadata().add('k');
+          var url = window.URL.createObjectURL(blob);
+          var worker = new Worker(url);
+          worker.postMessage({
+            scripts: morpheus.Util.getScriptPath(),
+            dataset: morpheus.Dataset.toJSON(subset, {
+              columnFields: [],
+              rowFields: [],
+              seriesIndices: [0]
+            }),
+            input: options.input
+          });
 
-          for (var i = 0, size = pvalueVector.size(); i < size; i++) {
-            pvalueVector.setValue(i, result.rowSpecificPValues[i]);
-            fdrVector.setValue(i, result.fdr[i]);
-            kVector.setValue(i, result.k[i]);
-            v.setValue(i, result.scores[i]);
-          }
-          vectors.push(pvalueVector);
-          vectors.push(fdrVector);
-          vectors.push(kVector);
-          done();
-          worker.terminate();
-          window.URL.revokeObjectURL(url);
-        };
-        return worker;
+          worker.onmessage = function (e) {
+            done(e.data);
+            worker.terminate();
+            window.URL.revokeObjectURL(url);
+          };
+          return worker;
+        } else {
+          done(morpheus.MarkerSelection.execute(subset, options.input));
+        }
+
       } else {
         for (var i = 0, size = dataset.getRowCount(); i < size; i++) {
           v.setValue(i, f(list1.setIndex(i), list2.setIndex(i)));

src/util/util.js
@@ -1453,6 +1453,34 @@ morpheus.Util.createValueToIndices = function (array, field) {
   return map;
 };
 
+morpheus.Util.createMorpheusHeader = function () {
+  var html = [];
+  html
+  .push('<div style="margin-bottom:10px;"><svg width="32px" height="32px"><g><rect x="0" y="0" width="32" height="14" style="fill:#ca0020;stroke:none"/><rect x="0" y="18" width="32" height="14" style="fill:#0571b0;stroke:none"/></g></svg> <div data-name="brand" style="display:inline-block; vertical-align: top;font-size:24px;font-family:sans-serif;">');
+  html.push('<span>M</span>');
+  html.push('<span>o</span>');
+  html.push('<span>r</span>');
+  html.push('<span>p</span>');
+  html.push('<span>h</span>');
+  html.push('<span>e</span>');
+  html.push('<span>u</span>');
+  html.push('<span>s</span>');
+  html.push('</span>');
+  html.push('</div>');
+  var $div = $(html.join(''));
+  var colorScale = d3.scale.linear().domain([0, 4, 7]).range(['#ca0020', '#999999', '#0571b0']).clamp(true);
+  var brands = $div.find('[data-name="brand"] > span');
+  var index = 0;
+  var step = function () {
+    brands[index].style.color = colorScale(index);
+    index++;
+    if (index < brands.length) {
+      setTimeout(step, 200);
+    }
+  };
+  setTimeout(step, 300);
+  return $div;
+};
 morpheus.Util.createLoadingEl = function () {
   return $('<div style="overflow:hidden;text-align:center;"><i class="fa fa-spinner fa-spin fa-3x"></i><span style="padding-left:4px;vertical-align:middle;font-weight:bold;">Loading...</span></div>');
 };

vlakam/phantasus.js

src/landing_page.js
@@ -8,21 +8,8 @@ phantasus.LandingPage = function (pageOptions) {
   var $el = $('<div class="container" style="display: none;"></div>');
   this.$el = $el;
   var html = [];
+  phantasus.Util.createPhantasusHeader().appendTo($el);
   html.push('<div data-name="help" class="pull-right"></div>');
-
-  html
-    .push('<div style="margin-bottom:10px;"><svg width="32px" height="32px"><g><rect x="0" y="0" width="32" height="14" style="fill:#ca0020;stroke:none"/><rect x="0" y="18" width="32" height="14" style="fill:#0571b0;stroke:none"/></g></svg> <div data-name="brand" style="display:inline-block; vertical-align: top;font-size:24px;font-family:sans-serif;">');
-  html.push('<span>P</span>');
-  html.push('<span>h</span>');
-  html.push('<span>a</span>');
-  html.push('<span>n</span>');
-  html.push('<span>t</span>');
-  html.push('<span>a</span>');
-  html.push('<span>s</span>');
-  html.push('<span>u</span>');
-  html.push('<span>s</span>');
-  html.push('</span>');
-  html.push('</div>');
 
   html.push('<h4>Open your own file</h4>');
   html.push('<div data-name="formRow" class="center-block"></div>');
@@ -30,8 +17,7 @@ phantasus.LandingPage = function (pageOptions) {
     ' dataset</h4></div>');
   html.push('</div>');
   var $html = $(html.join(''));
-  var colorScale = d3.scale.linear().domain([0, 4, 7]).range(['#ca0020', '#999999', '#0571b0']).clamp(true);
-  var brands = $html.find('[data-name="brand"] > span');
+
   $html.appendTo($el);
   new phantasus.HelpMenu().$el.appendTo($el.find('[data-name=help]'));
   var formBuilder = new phantasus.FormBuilder();
@@ -47,15 +33,7 @@ phantasus.LandingPage = function (pageOptions) {
   formBuilder.$form.appendTo($el.find('[data-name=formRow]'));
   this.formBuilder = formBuilder;
   this.$sampleDatasetsEl = $el.find('[data-name=preloadedDataset]');
-  var index = 0;
-  var step = function () {
-    brands[index].style.color = colorScale(index);
-    index++;
-    if (index < brands.length) {
-      setTimeout(step, 200);
-    }
-  };
-  setTimeout(step, 300);
+
   this.tabManager = new phantasus.TabManager({landingPage: this});
   this.tabManager.on('change rename add remove', function (e) {
     var title = _this.tabManager.getTabText(_this.tabManager.getActiveTabId());

src/tools/marker_selection.js
@@ -122,7 +122,7 @@ phantasus.MarkerSelection.prototype = {
         name: 'number_of_markers',
         value: '100',
         type: 'text',
-        help: 'The initial number of markers to show in each direction. Click <button title="Filter (Ctrl+L)" type="button" class="btn btn-default btn-xs dropdown-toggle"><span class="fa fa-filter"></span></button> to change.'
+        help: 'The initial number of markers to show in each direction.'
       }, {
         name: 'permutations',
         value: '0',
@@ -132,10 +132,20 @@ phantasus.MarkerSelection.prototype = {
   execute: function (options) {
 
     var project = options.project;
-    // classA and classB are arrays of identifiers if run via user
+    // classA and classB are arrays of Identifiers if run via user
     // interface. If run via JSON, will be string arrays
     var classA = options.input.class_a;
+    var classB = options.input.class_b;
+    if (classA.length === 0 && classB.length === 0) {
+      throw 'No samples in class A and class B';
+    }
 
+    if (classA.length === 0) {
+      throw 'No samples in class A';
+    }
+    if (classB.length === 0) {
+      throw 'No samples in class B';
+    }
     for (var i = 0; i < classA.length; i++) {
       var val = classA[i];
       if (!(val instanceof phantasus.Identifier)) {
@@ -143,7 +153,7 @@ phantasus.MarkerSelection.prototype = {
           phantasus.Util.isArray(val) ? val : [val]);
       }
     }
-    var classB = options.input.class_b;
+
     for (var i = 0; i < classB.length; i++) {
       var val = classB[i];
       if (!(val instanceof phantasus.Identifier)) {
@@ -152,18 +162,16 @@ phantasus.MarkerSelection.prototype = {
       }
     }
     var npermutations = parseInt(options.input.permutations);
+    var dataset = project.getSortedFilteredDataset();
+
     var fieldNames = options.input.field;
     if (!phantasus.Util.isArray(fieldNames)) {
       fieldNames = [fieldNames];
     }
-    var dataset = project.getSortedFilteredDataset();
+
     var vectors = phantasus.MetadataUtil.getVectors(dataset
-      .getColumnMetadata(), fieldNames);
-
+    .getColumnMetadata(), fieldNames);
     var idToIndices = phantasus.VectorUtil.createValuesToIndicesMap(vectors);
-    var f = phantasus.MarkerSelection.Functions
-      .fromString(options.input.metric);
-
     var aIndices = [];
     var bIndices = [];
     classA.forEach(function (id) {
@@ -181,16 +189,8 @@ phantasus.MarkerSelection.prototype = {
       bIndices = bIndices.concat(indices);
     });
 
-    if (aIndices.length === 0 && bIndices.length === 0) {
-      throw 'No samples in class A and class B';
-    }
-
-    if (aIndices.length === 0) {
-      throw 'No samples in class A';
-    }
-    if (bIndices.length === 0) {
-      throw 'No samples in class B';
-    }
+    var f = phantasus.MarkerSelection.Functions
+    .fromString(options.input.metric);
 
     var classASet = {};
     for (var i = 0; i < aIndices.length; i++) {
@@ -237,8 +237,20 @@ phantasus.MarkerSelection.prototype = {
     for (var i = 0; i < bIndices.length; i++) {
       comparisonVector.setValue(bIndices[i], 'B');
     }
-    function done() {
+    function done(result) {
+      var pvalueVector = dataset.getRowMetadata().add('p_value');
+      var fdrVector = dataset.getRowMetadata().add('FDR(BH)');
+      var kVector = dataset.getRowMetadata().add('k');
 
+      for (var i = 0, size = pvalueVector.size(); i < size; i++) {
+        pvalueVector.setValue(i, result.rowSpecificPValues[i]);
+        fdrVector.setValue(i, result.fdr[i]);
+        kVector.setValue(i, result.k[i]);
+        v.setValue(i, result.scores[i]);
+      }
+      vectors.push(pvalueVector);
+      vectors.push(fdrVector);
+      vectors.push(kVector);
       if (project.getRowFilter().getFilters().length > 0) {
         project.getRowFilter().setAnd(true, true);
       }
@@ -319,51 +331,42 @@ phantasus.MarkerSelection.prototype = {
       vectors.push(contingencyTableVector);
       done();
     } else {
-      // background
       if (npermutations > 0) {
-        options.input.numClassA = aIndices.length;
-        options.input.npermutations = npermutations;
-        var blob = new Blob(
-          ['self.onmessage = function(e) {'
-          + 'importScripts(e.data.scripts);'
-          + 'self.postMessage(phantasus.MarkerSelection.execute(phantasus.Dataset.fromJSON(e.data.dataset), e.data.input));'
-          + '}']);
-
-        var url = window.URL.createObjectURL(blob);
-        var worker = new Worker(url);
         var subset = new phantasus.SlicedDatasetView(dataset, null,
           aIndices.concat(bIndices));
 
-        worker.postMessage({
-          scripts: phantasus.Util.getScriptPath(),
-          dataset: phantasus.Dataset.toJSON(subset, {
-            columnFields: [],
-            rowFields: [],
-            seriesIndices: [0]
-          }),
-          input: options.input
-        });
+        options.input.background = options.input.background && typeof Worker !== 'undefined';
+        options.input.numClassA = aIndices.length;
+        options.input.npermutations = npermutations;
+        if (options.input.background) {
+          var blob = new Blob(
+            ['self.onmessage = function(e) {'
+            + 'importScripts(e.data.scripts);'
+            + 'self.postMessage(phantasus.MarkerSelection.execute(phantasus.Dataset.fromJSON(e.data.dataset), e.data.input));'
+            + '}']);
 
-        worker.onmessage = function (e) {
-          var result = e.data;
-          var pvalueVector = dataset.getRowMetadata().add('p_value');
-          var fdrVector = dataset.getRowMetadata().add('FDR(BH)');
-          var kVector = dataset.getRowMetadata().add('k');
+          var url = window.URL.createObjectURL(blob);
+          var worker = new Worker(url);
+          worker.postMessage({
+            scripts: phantasus.Util.getScriptPath(),
+            dataset: phantasus.Dataset.toJSON(subset, {
+              columnFields: [],
+              rowFields: [],
+              seriesIndices: [0]
+            }),
+            input: options.input
+          });
 
-          for (var i = 0, size = pvalueVector.size(); i < size; i++) {
-            pvalueVector.setValue(i, result.rowSpecificPValues[i]);
-            fdrVector.setValue(i, result.fdr[i]);
-            kVector.setValue(i, result.k[i]);
-            v.setValue(i, result.scores[i]);
-          }
-          vectors.push(pvalueVector);
-          vectors.push(fdrVector);
-          vectors.push(kVector);
-          done();
-          worker.terminate();
-          window.URL.revokeObjectURL(url);
-        };
-        return worker;
+          worker.onmessage = function (e) {
+            done(e.data);
+            worker.terminate();
+            window.URL.revokeObjectURL(url);
+          };
+          return worker;
+        } else {
+          done(phantasus.MarkerSelection.execute(subset, options.input));
+        }
+
       } else {
         for (var i = 0, size = dataset.getRowCount(); i < size; i++) {
           v.setValue(i, f(list1.setIndex(i), list2.setIndex(i)));

src/util/util.js
@@ -1456,6 +1456,35 @@ phantasus.Util.createValueToIndices = function (array, field) {
   return map;
 };
 
+phantasus.Util.createPhantasusHeader = function () {
+  var html = [];
+  html
+  .push('<div style="margin-bottom:10px;"><svg width="32px" height="32px"><g><rect x="0" y="0" width="32" height="14" style="fill:#ca0020;stroke:none"/><rect x="0" y="18" width="32" height="14" style="fill:#0571b0;stroke:none"/></g></svg> <div data-name="brand" style="display:inline-block; vertical-align: top;font-size:24px;font-family:sans-serif;">');
+  html.push('<span>P</span>');
+  html.push('<span>h</span>');
+  html.push('<span>a</span>');
+  html.push('<span>n</span>');
+  html.push('<span>t</span>');
+  html.push('<span>a</span>');
+  html.push('<span>s</span>');
+  html.push('<span>u</span>');
+  html.push('<span>s</span>');
+  html.push('</span>');
+  html.push('</div>');
+  var $div = $(html.join(''));
+  var colorScale = d3.scale.linear().domain([0, 4, 7]).range(['#ca0020', '#999999', '#0571b0']).clamp(true);
+  var brands = $div.find('[data-name="brand"] > span');
+  var index = 0;
+  var step = function () {
+    brands[index].style.color = colorScale(index);
+    index++;
+    if (index < brands.length) {
+      setTimeout(step, 200);
+    }
+  };
+  setTimeout(step, 300);
+  return $div;
+};
 phantasus.Util.createLoadingEl = function () {
   return $('<div style="overflow:hidden;text-align:center;"><i class="fa fa-spinner fa-spin fa-3x"></i><span style="padding-left:4px;vertical-align:middle;font-weight:bold;">Loading...</span></div>');
 };