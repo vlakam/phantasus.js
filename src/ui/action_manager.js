cmap/morpheus.js

fixed keyboard shortcuts

src/ui/action_manager.js
@@ -540,10 +540,8 @@ morpheus.ActionManager = function () {
   });
   this.add({
     name: 'Keymap Reference',
-    cb: function () {
-      new morpheus.HeatMapKeyListener({
-        $tabPanel: $()
-      }).showKeyMapReference();
+    cb: function (options) {
+      new morpheus.HeatMapKeyListener(options.heatMap).showKeyMapReference();
     }
   });
 

src/ui/heat_map_key_listener.js
@@ -389,8 +389,9 @@ morpheus.HeatMapKeyListener = function (heatMap) {
     if (sc.shiftKey) {
       s.push('Shift+');
     }
-    s.push(morpheus.KeyboardCharMap[sc.which[0]]);
-
+    if (sc.which) {
+      s.push(morpheus.KeyboardCharMap[sc.which[0]]);
+    }
     s.push('</b>');
     return s.join('');
   }

vlakam/phantasus.js

src/ui/action_manager.js
@@ -549,10 +549,8 @@ phantasus.ActionManager = function () {
   });
   this.add({
     name: 'Keymap Reference',
-    cb: function () {
-      new phantasus.HeatMapKeyListener({
-        $tabPanel: $()
-      }).showKeyMapReference();
+    cb: function (options) {
+      new phantasus.HeatMapKeyListener(options.heatMap).showKeyMapReference();
     }
   });
 

src/ui/heat_map_key_listener.js
@@ -389,8 +389,9 @@ phantasus.HeatMapKeyListener = function (heatMap) {
     if (sc.shiftKey) {
       s.push('Shift+');
     }
-    s.push(phantasus.KeyboardCharMap[sc.which[0]]);
-
+    if (sc.which) {
+      s.push(phantasus.KeyboardCharMap[sc.which[0]]);
+    }
     s.push('</b>');
     return s.join('');
   }