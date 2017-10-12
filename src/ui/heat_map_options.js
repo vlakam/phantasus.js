cmap/morpheus.js

number of fraction digits for heat map. Max element width

src/ui/heat_map_options.js
@@ -64,21 +64,20 @@ morpheus.HeatMapOptions = function (heatMap) {
     title: 'Size by minimum',
     required: true,
     type: 'text',
-    col: 'col-xs-4'
+    style: 'max-width: 100px;'
   });
   items.push({
     name: 'size_by_maximum',
     title: 'Size by maximum',
     required: true,
     type: 'text',
-    col: 'col-xs-4'
+    style: 'max-width: 100px;'
   });
 
   items.push({
     name: 'conditional_rendering',
     required: true,
-    type: 'button',
-    col: 'col-xs-4'
+    type: 'button'
   });
 
   var displayItems = [
@@ -87,7 +86,7 @@ morpheus.HeatMapOptions = function (heatMap) {
       name: 'link_rows_and_columns',
       required: true,
       type: 'checkbox',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: heatMap.getProject().isSymmetric()
     },
     {
@@ -100,21 +99,21 @@ morpheus.HeatMapOptions = function (heatMap) {
       name: 'grid_thickness',
       required: true,
       type: 'text',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: morpheus.Util.nf(heatMap.heatmap.getGridThickness())
     },
     {
       name: 'grid_color',
       required: true,
       type: 'color',
-      col: 'col-xs-2',
+      style: 'max-width: 50px;',
       value: heatMap.heatmap.getGridColor()
     },
     {
       name: 'row_size',
       required: true,
       type: 'text',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: morpheus.Util.nf(heatMap.heatmap.getRowPositions()
       .getSize())
     },
@@ -122,7 +121,7 @@ morpheus.HeatMapOptions = function (heatMap) {
       name: 'column_size',
       required: true,
       type: 'text',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: morpheus.Util.nf(heatMap.heatmap
       .getColumnPositions().getSize())
     }, {
@@ -130,6 +129,15 @@ morpheus.HeatMapOptions = function (heatMap) {
       required: true,
       type: 'checkbox',
       value: heatMap.heatmap.isDrawValues()
+    }, {
+      name: 'number_of_fraction_digits',
+      required: true,
+      type: 'number',
+      disabled: !heatMap.heatmap.isDrawValues(),
+      min: 0,
+      step: 1,
+      style: 'max-width: 100px;',
+      value: morpheus.Util.getNumberFormatPatternFractionDigits(heatMap.heatmap.getDrawValuesFormat().toJSON().pattern)
     }];
   if (heatMap.rowDendrogram) {
     displayItems
@@ -137,7 +145,7 @@ morpheus.HeatMapOptions = function (heatMap) {
       name: 'row_dendrogram_line_thickness',
       required: true,
       type: 'text',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: morpheus.Util
       .nf(heatMap.rowDendrogram ? heatMap.rowDendrogram.lineWidth
         : 1)
@@ -149,7 +157,7 @@ morpheus.HeatMapOptions = function (heatMap) {
       name: 'column_dendrogram_line_thickness',
       required: true,
       type: 'text',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: morpheus.Util
       .nf(heatMap.columnDendrogram ? heatMap.columnDendrogram.lineWidth
         : 1)
@@ -160,7 +168,7 @@ morpheus.HeatMapOptions = function (heatMap) {
     name: 'info_window',
     required: true,
     type: 'select',
-    col: 'col-xs-4',
+    style: 'max-width:130px;',
     options: [{
       name: 'Fixed To Top',
       value: 0
@@ -309,11 +317,27 @@ morpheus.HeatMapOptions = function (heatMap) {
     heatMap.revalidate();
     colorSchemeChooser.restoreCurrentValue();
   });
+  var $fractionDigits = displayFormBuilder.$form.find('[name=number_of_fraction_digits]');
   displayFormBuilder.$form.find('[name=show_values]').on('click', function (e) {
-    heatMap.heatmap.setDrawValues($(this).prop('checked'));
+    var drawValues = $(this).prop('checked');
+    heatMap.heatmap.setDrawValues(drawValues);
+    $fractionDigits.prop('disabled', !drawValues);
     heatMap.revalidate();
     colorSchemeChooser.restoreCurrentValue();
   });
+
+  $fractionDigits.on(
+    'keyup input', _.debounce(
+      function () {
+        var n = parseInt($(this)
+        .val());
+        if (n >= 0) {
+          heatMap.heatmap.setDrawValuesFormat(morpheus.Util.createNumberFormat('.' + n + 'f'));
+          heatMap.heatmap.setInvalid(true);
+          heatMap.heatmap.repaint();
+        }
+      }, 100));
+
   displayFormBuilder.$form.find('[name=inline_tooltip]').on('click',
     function (e) {
       heatMap.options.inlineTooltip = $(this).prop('checked');

src/ui/vector_track.js
@@ -1183,26 +1183,25 @@ morpheus.VectorTrack.prototype = {
             }
           }
           var pattern = formatter != null ? formatter.toJSON().pattern : '.2f';
-
           var formBuilder = new morpheus.FormBuilder();
           formBuilder.append({
             name: 'number_of_fraction_digits',
             type: 'number',
-            value: parseInt(pattern.substring(1, pattern.length - 1)),
+            value: morpheus.Util.getNumberFormatPatternFractionDigits(pattern),
             required: true,
-            col: 'col-xs-2'
+            style: 'max-width:10px;'
           });
           formBuilder.find('number_of_fraction_digits').on(
-            'keyup input',
-            function () {
-              var n = parseInt($(this)
-              .val());
-              if (n >= 0) {
-                vector.getProperties().set(morpheus.VectorKeys.FORMATTER, {pattern: '.' + n + 'f'});
-                _this.setInvalid(true);
-                _this.repaint();
-              }
-            });
+            'keyup input', _.debounce(
+              function () {
+                var n = parseInt($(this)
+                .val());
+                if (n >= 0) {
+                  vector.getProperties().set(morpheus.VectorKeys.FORMATTER, {pattern: '.' + n + 'f'});
+                  _this.setInvalid(true);
+                  _this.repaint();
+                }
+              }, 100));
           morpheus.FormBuilder.showInModal({
             title: 'Format',
             close: 'Close',
@@ -1309,7 +1308,7 @@ morpheus.VectorTrack.prototype = {
             type: 'color',
             value: _this.settings.barColor,
             required: true,
-            col: 'col-xs-2'
+            style: 'max-width:50px;'
           });
           formBuilder.find('bar_color').on(
             'change',
@@ -1332,7 +1331,7 @@ morpheus.VectorTrack.prototype = {
             type: 'text',
             value: _this.settings.colorBarSize,
             required: true,
-            col: 'col-xs-2'
+            style: 'max-width:50px;'
           });
           formBuilder.find('size').on(
             'change',

src/util/util.js
@@ -896,7 +896,7 @@ morpheus.Util.xlsxTo2dArray = function (options, callback) {
       type: 'bootstrap-select',
       options: sheetNames,
       required: true,
-      col: 'col-xs-2'
+      style: 'max-width:100px;'
     });
     morpheus.FormBuilder.showInModal({
       title: 'Choose Sheet',
@@ -937,7 +937,7 @@ morpheus.Util.xlsxTo1dArray = function (options, callback) {
       type: 'bootstrap-select',
       options: sheetNames,
       required: true,
-      col: 'col-xs-2'
+      style: 'max-width:100px;'
     });
 
     morpheus.FormBuilder.showOkCancel({
@@ -1072,6 +1072,11 @@ morpheus.Util.intFormat = function (n) {
 morpheus.Util._nf = typeof d3 !== 'undefined' ? d3.format('.2f') : function (d) {
   return '' + d;
 };
+
+morpheus.Util.getNumberFormatPatternFractionDigits = function (pattern) {
+  return parseInt(pattern.substring(1, pattern.length - 1));
+};
+
 morpheus.Util.nf = function (n) {
   // var str = (n < 1 && n > -1 && n.toPrecision !== undefined) ? n
   // .toPrecision(4) : morpheus.Util._nf(n);

vlakam/phantasus.js

src/ui/heat_map_options.js
@@ -64,21 +64,20 @@ phantasus.HeatMapOptions = function (heatMap) {
     title: 'Size by minimum',
     required: true,
     type: 'text',
-    col: 'col-xs-4'
+    style: 'max-width: 100px;'
   });
   items.push({
     name: 'size_by_maximum',
     title: 'Size by maximum',
     required: true,
     type: 'text',
-    col: 'col-xs-4'
+    style: 'max-width: 100px;'
   });
 
   items.push({
     name: 'conditional_rendering',
     required: true,
-    type: 'button',
-    col: 'col-xs-4'
+    type: 'button'
   });
 
   var displayItems = [
@@ -87,7 +86,7 @@ phantasus.HeatMapOptions = function (heatMap) {
       name: 'link_rows_and_columns',
       required: true,
       type: 'checkbox',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: heatMap.getProject().isSymmetric()
     },
     {
@@ -100,21 +99,21 @@ phantasus.HeatMapOptions = function (heatMap) {
       name: 'grid_thickness',
       required: true,
       type: 'text',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: phantasus.Util.nf(heatMap.heatmap.getGridThickness())
     },
     {
       name: 'grid_color',
       required: true,
       type: 'color',
-      col: 'col-xs-2',
+      style: 'max-width: 50px;',
       value: heatMap.heatmap.getGridColor()
     },
     {
       name: 'row_size',
       required: true,
       type: 'text',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: phantasus.Util.nf(heatMap.heatmap.getRowPositions()
         .getSize())
     },
@@ -122,7 +121,7 @@ phantasus.HeatMapOptions = function (heatMap) {
       name: 'column_size',
       required: true,
       type: 'text',
-      col: 'col-xs-4',
+      style: 'max-width: 100px;',
       value: phantasus.Util.nf(heatMap.heatmap
         .getColumnPositions().getSize())
     }, {
@@ -130,6 +129,15 @@ phantasus.HeatMapOptions = function (heatMap) {
       required: true,
       type: 'checkbox',
       value: heatMap.heatmap.isDrawValues()
+    }, {
+      name: 'number_of_fraction_digits',
+      required: true,
+      type: 'number',
+      disabled: !heatMap.heatmap.isDrawValues(),
+      min: 0,
+      step: 1,
+      style: 'max-width: 100px;',
+      value: phantasus.Util.getNumberFormatPatternFractionDigits(heatMap.heatmap.getDrawValuesFormat().toJSON().pattern)
     }];
   if (heatMap.rowDendrogram) {
     displayItems
@@ -137,7 +145,7 @@ phantasus.HeatMapOptions = function (heatMap) {
         name: 'row_dendrogram_line_thickness',
         required: true,
         type: 'text',
-        col: 'col-xs-4',
+        style: 'max-width: 100px;',
         value: phantasus.Util
           .nf(heatMap.rowDendrogram ? heatMap.rowDendrogram.lineWidth
             : 1)
@@ -149,7 +157,7 @@ phantasus.HeatMapOptions = function (heatMap) {
         name: 'column_dendrogram_line_thickness',
         required: true,
         type: 'text',
-        col: 'col-xs-4',
+        style: 'max-width: 100px;',
         value: phantasus.Util
           .nf(heatMap.columnDendrogram ? heatMap.columnDendrogram.lineWidth
             : 1)
@@ -160,7 +168,7 @@ phantasus.HeatMapOptions = function (heatMap) {
     name: 'info_window',
     required: true,
     type: 'select',
-    col: 'col-xs-4',
+    style: 'max-width:130px;',
     options: [{
       name: 'Fixed To Top',
       value: 0
@@ -309,11 +317,27 @@ phantasus.HeatMapOptions = function (heatMap) {
     heatMap.revalidate();
     colorSchemeChooser.restoreCurrentValue();
   });
+  var $fractionDigits = displayFormBuilder.$form.find('[name=number_of_fraction_digits]');
   displayFormBuilder.$form.find('[name=show_values]').on('click', function (e) {
-    heatMap.heatmap.setDrawValues($(this).prop('checked'));
+    var drawValues = $(this).prop('checked');
+    heatMap.heatmap.setDrawValues(drawValues);
+    $fractionDigits.prop('disabled', !drawValues);
     heatMap.revalidate();
     colorSchemeChooser.restoreCurrentValue();
   });
+
+  $fractionDigits.on(
+    'keyup input', _.debounce(
+      function () {
+        var n = parseInt($(this)
+        .val());
+        if (n >= 0) {
+          heatMap.heatmap.setDrawValuesFormat(phantasus.Util.createNumberFormat('.' + n + 'f'));
+          heatMap.heatmap.setInvalid(true);
+          heatMap.heatmap.repaint();
+        }
+      }, 100));
+
   displayFormBuilder.$form.find('[name=inline_tooltip]').on('click',
     function (e) {
       heatMap.options.inlineTooltip = $(this).prop('checked');

src/ui/vector_track.js
@@ -1183,26 +1183,25 @@ phantasus.VectorTrack.prototype = {
             }
           }
           var pattern = formatter != null ? formatter.toJSON().pattern : '.2f';
-
           var formBuilder = new phantasus.FormBuilder();
           formBuilder.append({
             name: 'number_of_fraction_digits',
             type: 'number',
-            value: parseInt(pattern.substring(1, pattern.length - 1)),
+            value: phantasus.Util.getNumberFormatPatternFractionDigits(pattern),
             required: true,
-            col: 'col-xs-2'
+            style: 'max-width:10px;'
           });
           formBuilder.find('number_of_fraction_digits').on(
-            'keyup input',
-            function () {
-              var n = parseInt($(this)
-              .val());
-              if (n >= 0) {
-                vector.getProperties().set(phantasus.VectorKeys.FORMATTER, {pattern: '.' + n + 'f'});
-                _this.setInvalid(true);
-                _this.repaint();
-              }
-            });
+            'keyup input', _.debounce(
+              function () {
+                var n = parseInt($(this)
+                .val());
+                if (n >= 0) {
+                  vector.getProperties().set(phantasus.VectorKeys.FORMATTER, {pattern: '.' + n + 'f'});
+                  _this.setInvalid(true);
+                  _this.repaint();
+                }
+              }, 100));
           phantasus.FormBuilder.showInModal({
             title: 'Format',
             close: 'Close',
@@ -1309,7 +1308,7 @@ phantasus.VectorTrack.prototype = {
             type: 'color',
             value: _this.settings.barColor,
             required: true,
-            col: 'col-xs-2'
+            style: 'max-width:50px;'
           });
           formBuilder.find('bar_color').on(
             'change',
@@ -1331,7 +1330,7 @@ phantasus.VectorTrack.prototype = {
             type: 'text',
             value: _this.settings.colorBarSize,
             required: true,
-            col: 'col-xs-2'
+            style: 'max-width:50px;'
           });
           formBuilder.find('size').on(
             'change',

src/util/util.js
@@ -896,7 +896,7 @@ phantasus.Util.xlsxTo2dArray = function (options, callback) {
       type: 'bootstrap-select',
       options: sheetNames,
       required: true,
-      col: 'col-xs-2'
+      style: 'max-width:100px;'
     });
     phantasus.FormBuilder.showInModal({
       title: 'Choose Sheet',
@@ -937,7 +937,7 @@ phantasus.Util.xlsxTo1dArray = function (options, callback) {
       type: 'bootstrap-select',
       options: sheetNames,
       required: true,
-      col: 'col-xs-2'
+      style: 'max-width:100px;'
     });
 
     phantasus.FormBuilder.showOkCancel({
@@ -1072,6 +1072,11 @@ phantasus.Util.intFormat = function (n) {
 phantasus.Util._nf = typeof d3 !== 'undefined' ? d3.format('.2f') : function (d) {
   return '' + d;
 };
+
+phantasus.Util.getNumberFormatPatternFractionDigits = function (pattern) {
+  return parseInt(pattern.substring(1, pattern.length - 1));
+};
+
 phantasus.Util.nf = function (n) {
   // var str = (n < 1 && n > -1 && n.toPrecision !== undefined) ? n
   // .toPrecision(4) : phantasus.Util._nf(n);