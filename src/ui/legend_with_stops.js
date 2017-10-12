cmap/morpheus.js

made it easier to drag color legend stops

src/ui/legend_with_stops.js
@@ -4,54 +4,44 @@ morpheus.LegendWithStops = function () {
     width: 300,
     height: 40
   });
-  var that = this;
-  this.hammer = morpheus.Util.hammer(this.canvas, ['pan', 'tap', 'press'])
+  var _this = this;
+  $(this.canvas).on('mousedown', function (event) {
+    var position = morpheus.CanvasUtil.getMousePos(
+      event.target, event);
+    _this.selectedIndex = _this
+    .findIndexForPosition(position);
+    _this.trigger('selectedIndex', {
+      selectedIndex: _this.selectedIndex
+    });
+  });
+  this.hammer = morpheus.Util.hammer(this.canvas, ['pan'])
   .on(
     'panmove',
     this.panmove = function (event) {
-      if (that.panStartSelectedIndex !== -1) {
+      if (_this.selectedIndex !== -1) {
         var position = morpheus.CanvasUtil.getMousePos(
           event.target, event);
-        var fraction = that.fractionToStopPix
+        var fraction = _this.fractionToStopPix
         .invert(position.x);
         fraction = Math.max(0, fraction);
         fraction = Math.min(1, fraction);
-        that.trigger('moved', {
+        _this.trigger('moved', {
           fraction: fraction
         });
       }
     }).on(
     'panstart',
     this.panstart = function (event) {
-      that.panStartSelectedIndex = that
-      .findIndexForPosition(morpheus.CanvasUtil
-      .getMousePos(event.target, event, true));
+      // _this.selectedIndex = _this
+      // .findIndexForPosition(morpheus.CanvasUtil
+      // .getMousePos(event.target, event, true));
     }).on('panend', this.panend = function (event) {
-    that.panStartSelectedIndex = -1;
-  }).on(
-    'tap',
-    this.tap = function (event) {
-      var position = morpheus.CanvasUtil.getMousePos(
-        event.target, event);
-      if (event.tapCount > 1) {
-        // don't add on double-click
-        // var fraction = that.fractionToStopPix
-        // .invert(position.x);
-        // that.trigger('added', {
-        //   fraction: fraction
-        // });
-      } else {
-        that.selectedIndex = that
-        .findIndexForPosition(position);
-        that.trigger('selectedIndex', {
-          selectedIndex: that.selectedIndex
-        });
-      }
-    });
+    _this.selectedIndex = -1;
+  });
   $(this.canvas).on('keydown', function (e) {
     // 8=backspace, 46=delete
-    if ((e.which == 8 || e.which == 46) && that.selectedIndex !== -1) {
-      that.trigger('delete');
+    if ((e.which == 8 || e.which == 46) && _this.selectedIndex !== -1) {
+      _this.trigger('delete');
       e.preventDefault();
       e.stopPropagation();
       e.stopImmediatePropagation();
@@ -59,18 +49,17 @@ morpheus.LegendWithStops = function () {
   });
 };
 morpheus.LegendWithStops.prototype = {
-  selectedIndex: -1,
   border: 7,
   stopHalfSize: 5,
-  panStartSelectedIndex: -1,
+  selectedIndex: -1,
   destroy: function () {
-    $(this.canvas).off('keyup');
+    $(this.canvas).off('keyup').off('mousedown');
     this.hammer.off('panstart',
-      this.panstart).off('panmove', this.panmove).off('tap', this.tap);
+      this.panstart).off('panmove', this.panmove);
     this.hammer.destroy();
   },
   setSelectedIndex: function (index) {
-    this.panStartSelectedIndex = -1;
+    this.selectedIndex = -1;
   },
   findIndexForPosition: function (position) {
     // pix - stopHalfSize to pix + stopHalfSize

vlakam/phantasus.js

src/ui/legend_with_stops.js
@@ -4,54 +4,44 @@ phantasus.LegendWithStops = function () {
     width: 300,
     height: 40
   });
-  var that = this;
-  this.hammer = phantasus.Util.hammer(this.canvas, ['pan', 'tap', 'press'])
-    .on(
-      'panmove',
-      this.panmove = function (event) {
-        if (that.panStartSelectedIndex !== -1) {
-          var position = phantasus.CanvasUtil.getMousePos(
-            event.target, event);
-          var fraction = that.fractionToStopPix
-            .invert(position.x);
-          fraction = Math.max(0, fraction);
-          fraction = Math.min(1, fraction);
-          that.trigger('moved', {
-            fraction: fraction
-          });
-        }
-      }).on(
-      'panstart',
-      this.panstart = function (event) {
-        that.panStartSelectedIndex = that
-          .findIndexForPosition(phantasus.CanvasUtil
-            .getMousePos(event.target, event, true));
-      }).on('panend', this.panend = function (event) {
-      that.panStartSelectedIndex = -1;
-    }).on(
-      'tap',
-      this.tap = function (event) {
+  var _this = this;
+  $(this.canvas).on('mousedown', function (event) {
+    var position = phantasus.CanvasUtil.getMousePos(
+      event.target, event);
+    _this.selectedIndex = _this
+    .findIndexForPosition(position);
+    _this.trigger('selectedIndex', {
+      selectedIndex: _this.selectedIndex
+    });
+  });
+  this.hammer = phantasus.Util.hammer(this.canvas, ['pan'])
+  .on(
+    'panmove',
+    this.panmove = function (event) {
+      if (_this.selectedIndex !== -1) {
         var position = phantasus.CanvasUtil.getMousePos(
           event.target, event);
-        if (event.tapCount > 1) {
-          // don't add on double-click
-          // var fraction = that.fractionToStopPix
-          // .invert(position.x);
-          // that.trigger('added', {
-          //   fraction: fraction
-          // });
-        } else {
-          that.selectedIndex = that
-            .findIndexForPosition(position);
-          that.trigger('selectedIndex', {
-            selectedIndex: that.selectedIndex
-          });
-        }
-      });
+        var fraction = _this.fractionToStopPix
+        .invert(position.x);
+        fraction = Math.max(0, fraction);
+        fraction = Math.min(1, fraction);
+        _this.trigger('moved', {
+          fraction: fraction
+        });
+      }
+    }).on(
+    'panstart',
+    this.panstart = function (event) {
+      // _this.selectedIndex = _this
+      // .findIndexForPosition(phantasus.CanvasUtil
+      // .getMousePos(event.target, event, true));
+    }).on('panend', this.panend = function (event) {
+    _this.selectedIndex = -1;
+  });
   $(this.canvas).on('keydown', function (e) {
     // 8=backspace, 46=delete
-    if ((e.which == 8 || e.which == 46) && that.selectedIndex !== -1) {
-      that.trigger('delete');
+    if ((e.which == 8 || e.which == 46) && _this.selectedIndex !== -1) {
+      _this.trigger('delete');
       e.preventDefault();
       e.stopPropagation();
       e.stopImmediatePropagation();
@@ -59,18 +49,17 @@ phantasus.LegendWithStops = function () {
   });
 };
 phantasus.LegendWithStops.prototype = {
-  selectedIndex: -1,
   border: 7,
   stopHalfSize: 5,
-  panStartSelectedIndex: -1,
+  selectedIndex: -1,
   destroy: function () {
-    $(this.canvas).off('keyup');
+    $(this.canvas).off('keyup').off('mousedown');
     this.hammer.off('panstart',
-      this.panstart).off('panmove', this.panmove).off('tap', this.tap);
+      this.panstart).off('panmove', this.panmove);
     this.hammer.destroy();
   },
   setSelectedIndex: function (index) {
-    this.panStartSelectedIndex = -1;
+    this.selectedIndex = -1;
   },
   findIndexForPosition: function (position) {
     // pix - stopHalfSize to pix + stopHalfSize