cmap/morpheus.js

log fold change

src/matrix/bivariate_functions.js
@@ -176,6 +176,15 @@ morpheus.FoldChange = function (list1, list2) {
 morpheus.FoldChange.toString = function () {
   return 'Fold Change';
 };
+
+morpheus.LogFoldChange = function (list1, list2) {
+  var m1 = morpheus.Mean(list1);
+  var m2 = morpheus.Mean(list2);
+  return (m1 - m2);
+};
+morpheus.LogFoldChange.toString = function () {
+  return 'Log Fold Change';
+};
 morpheus.TTest = function (list1, list2) {
   var m1 = morpheus.Mean(list1);
   var m2 = morpheus.Mean(list2);

src/tools/marker_selection.js
@@ -6,7 +6,7 @@ morpheus.MarkerSelection = function () {
  * @private
  */
 morpheus.MarkerSelection.Functions = [morpheus.FisherExact,
-  morpheus.FoldChange, morpheus.SignalToNoise,
+  morpheus.FoldChange, morpheus.LogFoldChange, morpheus.SignalToNoise,
   morpheus.createSignalToNoiseAdjust(), morpheus.TTest];
 
 morpheus.MarkerSelection.Functions.fromString = function (s) {
@@ -202,8 +202,8 @@ morpheus.MarkerSelection.prototype = {
       }
     }
     var isFishy = f.toString() === morpheus.FisherExact.toString();
-    if (aIndices.length === 1 || bIndices.length === 1
-      && !(f instanceof morpheus.FisherExact)) {
+    if ((aIndices.length === 1 || bIndices.length === 1)
+      && !isFishy && f.toString() !== morpheus.LogFoldChange.toString()) {
       f = morpheus.FoldChange;
     }
     var list1 = new morpheus.DatasetRowView(new morpheus.SlicedDatasetView(

vlakam/phantasus.js

src/matrix/bivariate_functions.js
@@ -176,6 +176,15 @@ phantasus.FoldChange = function (list1, list2) {
 phantasus.FoldChange.toString = function () {
   return 'Fold Change';
 };
+
+phantasus.LogFoldChange = function (list1, list2) {
+  var m1 = phantasus.Mean(list1);
+  var m2 = phantasus.Mean(list2);
+  return (m1 - m2);
+};
+phantasus.LogFoldChange.toString = function () {
+  return 'Log Fold Change';
+};
 phantasus.TTest = function (list1, list2) {
   var m1 = phantasus.Mean(list1);
   var m2 = phantasus.Mean(list2);

src/tools/marker_selection.js
@@ -6,7 +6,7 @@ phantasus.MarkerSelection = function () {
  * @private
  */
 phantasus.MarkerSelection.Functions = [phantasus.FisherExact,
-  phantasus.FoldChange, phantasus.SignalToNoise,
+  phantasus.FoldChange, phantasus.LogFoldChange, phantasus.SignalToNoise,
   phantasus.createSignalToNoiseAdjust(), phantasus.TTest];
 
 phantasus.MarkerSelection.Functions.fromString = function (s) {