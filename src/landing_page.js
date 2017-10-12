cmap/morpheus.js

show example datasets after showing file browser

src/landing_page.js
@@ -95,16 +95,6 @@ morpheus.LandingPage.prototype = {
   },
   show: function () {
     var _this = this;
-    if (navigator.onLine && !this.sampleDatasets) {
-      this.sampleDatasets = new morpheus.SampleDatasets({
-        $el: this.$sampleDatasetsEl,
-        show: true,
-        callback: function (heatMapOptions) {
-          _this.open(heatMapOptions);
-        }
-      });
-    }
-
     this.$el.show();
 
     this.formBuilder.on('change', function (e) {
@@ -159,6 +149,15 @@ morpheus.LandingPage.prototype = {
           _this.openFile(url);
         }
       });
+    if (navigator.onLine && !this.sampleDatasets) {
+      this.sampleDatasets = new morpheus.SampleDatasets({
+        $el: this.$sampleDatasetsEl,
+        show: true,
+        callback: function (heatMapOptions) {
+          _this.open(heatMapOptions);
+        }
+      });
+    }
   },
   openFile: function (value) {
     var _this = this;

vlakam/phantasus.js

src/landing_page.js
@@ -169,16 +169,6 @@ phantasus.LandingPage.prototype = {
   },
   show: function () {
     var _this = this;
-    if (navigator.onLine && !this.sampleDatasets) {
-      this.sampleDatasets = new phantasus.SampleDatasets({
-        $el: this.$sampleDatasetsEl,
-        show: true,
-        callback: function (heatMapOptions) {
-          _this.open(heatMapOptions);
-        }
-      });
-    }
-
     this.$el.show();
 
     this.formBuilder.on('change', function (e) {
@@ -233,6 +223,15 @@ phantasus.LandingPage.prototype = {
           _this.openFile(url);
         }
       });
+    if (navigator.onLine && !this.sampleDatasets) {
+      this.sampleDatasets = new phantasus.SampleDatasets({
+        $el: this.$sampleDatasetsEl,
+        show: true,
+        callback: function (heatMapOptions) {
+          _this.open(heatMapOptions);
+        }
+      });
+    }
   },
   openFile: function (value) {
     var _this = this;