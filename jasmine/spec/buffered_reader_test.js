cmap/morpheus.js

Added polyfills for phantomjs

jasmine/spec/buffered_reader_test.js
@@ -11,7 +11,7 @@ describe('buffered_reader_test', function () {
   }
 
   it('test_chunk', function (done) {
-    if (morpheus.Util.isFetchSupported()) {
+    if (morpheus.Util.isFetchStreamingSupported()) {
       var url = 'test_files/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf.txt';
       var lines = [];
       var splitLines = null;

vlakam/phantasus.js

jasmine/spec/buffered_reader_test.js
@@ -11,7 +11,7 @@ describe('buffered_reader_test', function () {
   }
 
   it('test_chunk', function (done) {
-    if (phantasus.Util.isFetchSupported()) {
+    if (phantasus.Util.isFetchStreamingSupported()) {
       var url = 'test_files/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf.txt';
       var lines = [];
       var splitLines = null;