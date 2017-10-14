cmap/morpheus.js

use latest CCLE

src/sample_datasets.js
@@ -51,21 +51,7 @@ morpheus.SampleDatasets = function (options) {
   }).then(function (text) {
     var exampleHtml = [];
     var id = _.uniqueId('morpheus');
-    // exampleHtml.push('<a data-toggle="collapse" href="#' + id + '" aria-expanded="false" aria-controls="' + id +
-    //   '">Other</a>');
-    // exampleHtml.push('<hr>');
-    // exampleHtml.push('<div class="collapse" id="' + id + '">');
-    // exampleHtml.push('<label style="display: inline;">Motor Trend Car Road Tests</label>');
-    // exampleHtml.push('<div style="margin: 6px 0 0 20px;display: inline;vertical-align:' +
-    //   ' top;">');
-    // exampleHtml.push('<button type="button" class="btn btn-default" name="ccle">Open</button>');
-    // exampleHtml.push('</div>');
-    // exampleHtml.push('<div></div><small>The data was extracted from 1974' +
-    //   ' Motor Trend US magazine, and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973–74 models).</small></div>');
-    //
-    // exampleHtml.push('</div>');
-    //
-    // id = _.uniqueId('morpheus');
+
     exampleHtml.push('<a data-toggle="collapse" href="#' + id + '" aria-expanded="false" aria-controls="' + id +
       '">Cancer Cell Line Encyclopedia (CCLE), Project Achilles</a>');
     exampleHtml.push('<div class="collapse" id="' + id + '">');
@@ -218,65 +204,35 @@ morpheus.SampleDatasets.getCCLEDataset = function (options) {
   var datasets = [];
   if (options.sig_genes) {
     datasets.push(
-      'https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf.txt');
+      'https://software.broadinstitute.org/morpheus/preloaded-datasets/ccle2maf_081117.maf.txt');
   }
   if (options.cn) {
     datasets.push('https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_copynumber_byGene_2013-12-03.gct');
   }
 
   if (options.mrna) {
-    datasets.push('https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_Expression_Entrez_2012-09-29.txt');
+    datasets.push('https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_expression_081117.rpkm.gct');
   }
   if (options.ach) {
     datasets.push('https://software.broadinstitute.org/morpheus/preloaded-datasets/Achilles_QC_v2.4.3.rnai.Gs.gct');
   }
-  var columnAnnotations = [];
-  if (options.ach) {
-    // there are several cell lines that are in Achilles but not CCLE
-    columnAnnotations.push({
-      file: 'https://software.broadinstitute.org/morpheus/preloaded-datasets/Achilles_v2.4_SampleInfo_small.txt',
-      datasetField: 'id',
-      fileField: 'id'
-    });
 
-  }
-  columnAnnotations.push({
-    file: 'https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_Sample_Info.txt',
-    datasetField: 'id',
-    fileField: 'id'
+  var d = $.Deferred();
+  var datasetPromise = morpheus.DatasetUtil.readDatasetArray(datasets);
+  datasetPromise.done(function (dataset) {
+    var idVector = dataset.getColumnMetadata().get(0);
+    var siteVector = dataset.getColumnMetadata().add('site');
+    for (var j = 0, ncols = siteVector.size(); j < ncols; j++) {
+      var id = idVector.getValue(j);
+      var index = id.indexOf('_');
+      idVector.setValue(j, id.substring(0, index));
+      siteVector.setValue(j, id.substring(index + 1));
+    }
+    d.resolve(dataset);
+  }).fail(function (err) {
+    d.reject(err);
   });
-
-  var returnDeferred = $.Deferred();
-  var datasetDef = morpheus.DatasetUtil.readDatasetArray(datasets);
-
-  var annotationDef = morpheus.DatasetUtil.annotate({
-    annotations: columnAnnotations,
-    isColumns: true
-  });
-  var datasetToReturn;
-  datasetDef.done(function (d) {
-    datasetToReturn = d;
-  });
-  datasetDef.fail(function (message) {
-    returnDeferred.reject(message);
-  });
-  var annotationCallbacks;
-  annotationDef.done(function (callbacks) {
-    annotationCallbacks = callbacks;
-  });
-  annotationDef.fail(function (message) {
-    returnDeferred.reject(message);
-  });
-
-  $.when.apply($, [datasetDef, annotationDef]).then(function () {
-
-    annotationCallbacks.forEach(function (f) {
-      f(datasetToReturn);
-    });
-    returnDeferred.resolve(datasetToReturn);
-  });
-
-  return returnDeferred;
+  return d;
 };
 morpheus.SampleDatasets.prototype = {
 
@@ -331,7 +287,10 @@ morpheus.SampleDatasets.prototype = {
       rows: [
         {
           field: 'id',
-          display: 'text,tooltip'
+          display: 'text'
+        }, {
+          field: 'Description',
+          display: 'text, tooltip'
         }, {
           field: 'mutation_summary',
           display: 'stacked_bar'
@@ -347,16 +306,7 @@ morpheus.SampleDatasets.prototype = {
           field: 'mutation_summary',
           display: 'stacked_bar'
         }, {
-          field: 'gender',
-          display: 'color, highlight'
-        }, {
-          field: 'histology',
-          display: 'color, highlight'
-        }, {
-          field: 'histology subtype',
-          display: 'color, highlight'
-        }, {
-          field: 'primary_site',
+          field: 'site',
           display: 'color, highlight'
         }],
       dataset: morpheus.SampleDatasets.getCCLEDataset(options)

vlakam/phantasus.js

src/sample_datasets.js
@@ -247,66 +247,35 @@ phantasus.SampleDatasets.getCCLEDataset = function (options) {
   var datasets = [];
   if (options.sig_genes) {
     datasets.push(
-      'https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_hybrid_capture1650_hg19_NoCommonSNPs_NoNeutralVariants_CDS_2012.05.07.maf.txt');
+      'https://software.broadinstitute.org/morpheus/preloaded-datasets/ccle2maf_081117.maf.txt');
   }
   if (options.cn) {
     datasets.push('https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_copynumber_byGene_2013-12-03.gct');
   }
 
   if (options.mrna) {
-    datasets.push('https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_Expression_Entrez_2012-09-29.txt');
+    datasets.push('https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_expression_081117.rpkm.gct');
   }
   if (options.ach) {
     datasets.push('https://software.broadinstitute.org/morpheus/preloaded-datasets/Achilles_QC_v2.4.3.rnai.Gs.gct');
   }
-  var columnAnnotations = [];
-  if (options.ach) {
-    // there are several cell lines that are in Achilles but not CCLE
-    columnAnnotations.push({
-      file: 'https://software.broadinstitute.org/morpheus/preloaded-datasets/Achilles_v2.4_SampleInfo_small.txt',
-      datasetField: 'id',
-      fileField: 'id'
-    });
 
-  }
-  columnAnnotations.push({
-    file: 'https://software.broadinstitute.org/morpheus/preloaded-datasets/CCLE_Sample_Info.txt',
-    datasetField: 'id',
-    fileField: 'id'
+  var d = $.Deferred();
+  var datasetPromise = phantasus.DatasetUtil.readDatasetArray(datasets);
+  datasetPromise.done(function (dataset) {
+    var idVector = dataset.getColumnMetadata().get(0);
+    var siteVector = dataset.getColumnMetadata().add('site');
+    for (var j = 0, ncols = siteVector.size(); j < ncols; j++) {
+      var id = idVector.getValue(j);
+      var index = id.indexOf('_');
+      idVector.setValue(j, id.substring(0, index));
+      siteVector.setValue(j, id.substring(index + 1));
+    }
+    d.resolve(dataset);
+  }).fail(function (err) {
+    d.reject(err);
   });
-
-  var returnDeferred = $.Deferred();
-  var datasetDef = phantasus.DatasetUtil.readDatasetArray(datasets);
-
-  var annotationDef = phantasus.DatasetUtil.annotate({
-    annotations: columnAnnotations,
-    isColumns: true
-  });
-  var datasetToReturn;
-  datasetDef.done(function (d) {
-    datasetToReturn = d;
-  });
-  datasetDef.fail(function (message) {
-    returnDeferred.reject(message);
-  });
-  var annotationCallbacks;
-  annotationDef.done(function (callbacks) {
-    annotationCallbacks = callbacks;
-  });
-  annotationDef.fail(function (message) {
-    returnDeferred.reject(message);
-  });
-
-  $.when.apply($, [datasetDef, annotationDef]).then(function () {
-
-    annotationCallbacks.forEach(function (f) {
-      f(datasetToReturn);
-    });
-    phantasus.DatasetUtil.toESSessionPromise(datasetToReturn);
-    returnDeferred.resolve(datasetToReturn);
-  });
-
-  return returnDeferred;
+  return d;
 };
 phantasus.SampleDatasets.prototype = {
 
@@ -362,7 +331,10 @@ phantasus.SampleDatasets.prototype = {
       rows: [
         {
           field: 'id',
-          display: 'text,tooltip'
+          display: 'text'
+        }, {
+          field: 'Description',
+          display: 'text, tooltip'
         }, {
           field: 'mutation_summary',
           display: 'stacked_bar'
@@ -378,16 +350,7 @@ phantasus.SampleDatasets.prototype = {
           field: 'mutation_summary',
           display: 'stacked_bar'
         }, {
-          field: 'gender',
-          display: 'color, highlight'
-        }, {
-          field: 'histology',
-          display: 'color, highlight'
-        }, {
-          field: 'histology subtype',
-          display: 'color, highlight'
-        }, {
-          field: 'primary_site',
+          field: 'site',
           display: 'color, highlight'
         }],
       dataset: phantasus.SampleDatasets.getCCLEDataset(options)