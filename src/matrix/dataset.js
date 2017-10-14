cmap/morpheus.js

save session when row # is showing bug fix

src/matrix/dataset.js
@@ -18,7 +18,7 @@ morpheus.Dataset = function (options) {
 
   this.seriesNames.push(options.name);
   this.seriesArrays.push(options.array ? options.array : morpheus.Dataset
-  .createArray(options));
+    .createArray(options));
   this.seriesDataTypes.push(options.dataType);
 };
 /**
@@ -52,7 +52,7 @@ morpheus.Dataset.toJSON = function (dataset, options) {
       }
     }
   }
-  var vectortoJSON = function (vector) {
+  var vectorToJSON = function (vector) {
     var array = [];
     for (var i = 0, size = vector.size(); i < size; i++) {
       array[i] = vector.getValue(i);
@@ -80,12 +80,14 @@ morpheus.Dataset.toJSON = function (dataset, options) {
     }
     for (var i = 0, count = metadata.getMetadataCount(); i < count; i++) {
       var v = metadata.get(i);
-      if (filter) {
-        if (filter.has(v.getName())) {
-          vectors.push(vectortoJSON(v));
+      if (!v.getProperties().has(morpheus.VectorKeys.IS_INDEX)) {
+        if (filter) {
+          if (filter.has(v.getName())) {
+            vectors.push(vectorToJSON(v));
+          }
+        } else {
+          vectors.push(vectorToJSON(v));
         }
-      } else {
-        vectors.push(vectortoJSON(v));
       }
     }
     return vectors;

src/matrix/vector_keys.js
@@ -28,6 +28,9 @@ morpheus.VectorKeys.RECOMPUTE_FUNCTION_SELECTION = 'morpheus.recompute.funct.sel
 /**Number format spec/function */
 morpheus.VectorKeys.FORMATTER = 'morpheus.formatter';
 
+/* Indicates that a "fake" vector to show row/column number */
+morpheus.VectorKeys.IS_INDEX = 'morpheus.isIndex';
+
 /** Whether vector values should be treated discretely or continuously */
 morpheus.VectorKeys.DISCRETE = 'morpheus.discrete';
 

src/ui/heat_map.js
@@ -1117,20 +1117,18 @@ morpheus.HeatMap.prototype = {
     json.showRowNumber = this.isShowRowNumber();
 
     // annotation shapes
-    json.rowShapeModel = this.getProject().getRowShapeModel().toJSON(this.rowTracks);
-    json.columnShapeModel = this.getProject().getColumnShapeModel().toJSON(this.columnTracks);
+    json.rowShapeModel = this.getProject().getRowShapeModel().toJSON(this.getVisibleTracks(false));
+    json.columnShapeModel = this.getProject().getColumnShapeModel().toJSON(this.getVisibleTracks(true));
 
     // annotation font
-    json.rowFontModel = this.getProject().getRowFontModel().toJSON(this.rowTracks);
-    json.columnFontModel = this.getProject().getColumnFontModel().toJSON(this.columnTracks);
+    json.rowFontModel = this.getProject().getRowFontModel().toJSON(this.getVisibleTracks(false));
+    json.columnFontModel = this.getProject().getColumnFontModel().toJSON(this.getVisibleTracks(true));
 
     // annotation colors
-    json.rowColorModel = this.getProject().getRowColorModel().toJSON(this.rowTracks);
-    json.columnColorModel = this.getProject().getColumnColorModel().toJSON(this.columnTracks);
+    json.rowColorModel = this.getProject().getRowColorModel().toJSON(this.getVisibleTracks(false));
+    json.columnColorModel = this.getProject().getColumnColorModel().toJSON(this.getVisibleTracks(true));
     // annotation display
-    json.rows = this.rowTracks.filter(function (track) {
-      return track.isVisible();
-    }).map(function (track) {
+    json.rows = this.getVisibleTracks(false).map(function (track) {
       var size = morpheus.CanvasUtil.getPreferredSize(_this.getTrackHeaderByIndex(_this.getTrackIndex(track.getName(), false), false));
       var obj = track.settings;
       obj.field = track.getName();
@@ -1139,9 +1137,7 @@ morpheus.HeatMap.prototype = {
       };
       return obj;
     });
-    json.columns = this.columnTracks.filter(function (track) {
-      return track.isVisible();
-    }).map(function (track) {
+    json.columns = this.getVisibleTracks(true).map(function (track) {
       var size = morpheus.CanvasUtil.getPreferredSize(_this.getTrackHeaderByIndex(_this.getTrackIndex(track.getName(), true), true));
       var obj = track.settings;
       obj.field = track.getName();
@@ -3162,14 +3158,15 @@ morpheus.HeatMap.prototype = {
   }
   ,
   getVisibleTrackNames: function (isColumns) {
-    var names = [];
+    this.getVisibleTracks(isColumns).map(function (track) {
+      return track.name;
+    });
+  },
+  getVisibleTracks: function (isColumns) {
     var tracks = isColumns ? this.columnTracks : this.rowTracks;
-    for (var i = 0, length = tracks.length; i < length; i++) {
-      if (tracks[i].isVisible() && tracks[i].getFullVector() != null) { // don't return row #
-        names.push(tracks[i].name);
-      }
-    }
-    return names;
+    return tracks.filter(function (track) {
+      return track.isVisible() && !track.getFullVector().getProperties().has(morpheus.VectorKeys.IS_INDEX);
+    });
   },
   isShowRowNumber: function () {
     return this.options.showRowNumber;
@@ -3189,7 +3186,12 @@ morpheus.HeatMap.prototype = {
         return v;
       };
       track.getFullVector = function () {
-
+        var v = new morpheus.AbstractVector('#', this.project.getFullDataset().getRowCount());
+        v.getProperties().set(morpheus.VectorKeys.FORMATTER, {pattern: 'i'});
+        v.getValue = function (index) {
+          return index + 1;
+        };
+        return v;
       };
 
       track.showPopup = function (e, isHeader) {

vlakam/phantasus.js

src/matrix/dataset.js
@@ -60,7 +60,7 @@ phantasus.Dataset.toJSON = function (dataset, options) {
       }
     }
   }
-  var vectortoJSON = function (vector) {
+  var vectorToJSON = function (vector) {
     var array = [];
     for (var i = 0, size = vector.size(); i < size; i++) {
       array[i] = vector.getValue(i);
@@ -88,12 +88,14 @@ phantasus.Dataset.toJSON = function (dataset, options) {
     }
     for (var i = 0, count = metadata.getMetadataCount(); i < count; i++) {
       var v = metadata.get(i);
-      if (filter) {
-        if (filter.has(v.getName())) {
-          vectors.push(vectortoJSON(v));
+      if (!v.getProperties().has(phantasus.VectorKeys.IS_INDEX)) {
+        if (filter) {
+          if (filter.has(v.getName())) {
+            vectors.push(vectorToJSON(v));
+          }
+        } else {
+          vectors.push(vectorToJSON(v));
         }
-      } else {
-        vectors.push(vectortoJSON(v));
       }
     }
     return vectors;

src/matrix/vector_keys.js
@@ -31,6 +31,9 @@ phantasus.VectorKeys.RECOMPUTE_FUNCTION_SELECTION = 'phantasus.recompute.funct.s
 /**Number format spec/function */
 phantasus.VectorKeys.FORMATTER = 'phantasus.formatter';
 
+/* Indicates that a "fake" vector to show row/column number */
+phantasus.VectorKeys.IS_INDEX = 'phantasus.isIndex';
+
 /** Whether vector values should be treated discretely or continuously */
 phantasus.VectorKeys.DISCRETE = 'phantasus.discrete';
 

src/ui/heat_map.js
@@ -1168,20 +1168,18 @@ phantasus.HeatMap.prototype = {
     json.showRowNumber = this.isShowRowNumber();
 
     // annotation shapes
-    json.rowShapeModel = this.getProject().getRowShapeModel().toJSON(this.rowTracks);
-    json.columnShapeModel = this.getProject().getColumnShapeModel().toJSON(this.columnTracks);
+    json.rowShapeModel = this.getProject().getRowShapeModel().toJSON(this.getVisibleTracks(false));
+    json.columnShapeModel = this.getProject().getColumnShapeModel().toJSON(this.getVisibleTracks(true));
 
     // annotation font
-    json.rowFontModel = this.getProject().getRowFontModel().toJSON(this.rowTracks);
-    json.columnFontModel = this.getProject().getColumnFontModel().toJSON(this.columnTracks);
+    json.rowFontModel = this.getProject().getRowFontModel().toJSON(this.getVisibleTracks(false));
+    json.columnFontModel = this.getProject().getColumnFontModel().toJSON(this.getVisibleTracks(true));
 
     // annotation colors
-    json.rowColorModel = this.getProject().getRowColorModel().toJSON(this.rowTracks);
-    json.columnColorModel = this.getProject().getColumnColorModel().toJSON(this.columnTracks);
+    json.rowColorModel = this.getProject().getRowColorModel().toJSON(this.getVisibleTracks(false));
+    json.columnColorModel = this.getProject().getColumnColorModel().toJSON(this.getVisibleTracks(true));
     // annotation display
-    json.rows = this.rowTracks.filter(function (track) {
-      return track.isVisible();
-    }).map(function (track) {
+    json.rows = this.getVisibleTracks(false).map(function (track) {
       var size = phantasus.CanvasUtil.getPreferredSize(_this.getTrackHeaderByIndex(_this.getTrackIndex(track.getName(), false), false));
       var obj = track.settings;
       obj.field = track.getName();
@@ -1190,9 +1188,7 @@ phantasus.HeatMap.prototype = {
       };
       return obj;
     });
-    json.columns = this.columnTracks.filter(function (track) {
-      return track.isVisible();
-    }).map(function (track) {
+    json.columns = this.getVisibleTracks(true).map(function (track) {
       var size = phantasus.CanvasUtil.getPreferredSize(_this.getTrackHeaderByIndex(_this.getTrackIndex(track.getName(), true), true));
       var obj = track.settings;
       obj.field = track.getName();
@@ -1776,7 +1772,7 @@ phantasus.HeatMap.prototype = {
       // see if default fields found
       if (!displaySpecified) {
         var defaultFieldsToShow = new phantasus.Set();
-        ['pert_iname', 'moa', 'target', 'description', 'cell_id', 'pert_type'].forEach(function (field) {
+        ['pert_iname', 'moa', 'target', 'cell_id', 'pert_type'].forEach(function (field) {
           defaultFieldsToShow.add(field);
         });
         for (var i = 0, metadataCount = displayMetadata.getMetadataCount(); i < metadataCount; i++) {
@@ -3214,14 +3210,15 @@ phantasus.HeatMap.prototype = {
   }
   ,
   getVisibleTrackNames: function (isColumns) {
-    var names = [];
+    this.getVisibleTracks(isColumns).map(function (track) {
+      return track.name;
+    });
+  },
+  getVisibleTracks: function (isColumns) {
     var tracks = isColumns ? this.columnTracks : this.rowTracks;
-    for (var i = 0, length = tracks.length; i < length; i++) {
-      if (tracks[i].isVisible() && tracks[i].getFullVector() != null) { // don't return row #
-        names.push(tracks[i].name);
-      }
-    }
-    return names;
+    return tracks.filter(function (track) {
+      return track.isVisible() && !track.getFullVector().getProperties().has(phantasus.VectorKeys.IS_INDEX);
+    });
   },
   isShowRowNumber: function () {
     return this.options.showRowNumber;
@@ -3241,7 +3238,12 @@ phantasus.HeatMap.prototype = {
         return v;
       };
       track.getFullVector = function () {
-
+        var v = new phantasus.AbstractVector('#', this.project.getFullDataset().getRowCount());
+        v.getProperties().set(phantasus.VectorKeys.FORMATTER, {pattern: 'i'});
+        v.getValue = function (index) {
+          return index + 1;
+        };
+        return v;
       };
 
       track.showPopup = function (e, isHeader) {