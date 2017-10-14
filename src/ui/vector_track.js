cmap/morpheus.js

fixed text config

src/ui/vector_track.js
@@ -238,11 +238,11 @@ morpheus.VectorTrack.prototype = {
           delete settings.render;
         }
       }
-      if (!this.isRenderAs(morpheus.VectorTrack.RENDER.TEXT) &&
-        (this.isRenderAs(morpheus.VectorTrack.RENDER.TEXT_AND_COLOR) || this.isRenderAs(morpheus.VectorTrack.RENDER.TEXT_AND_FONT))) {
-        settings.display.push(morpheus.VectorTrack.RENDER.TEXT);
-      }
       this.settings = settings;
+    }
+    if (!this.isRenderAs(morpheus.VectorTrack.RENDER.TEXT) &&
+      (this.isRenderAs(morpheus.VectorTrack.RENDER.TEXT_AND_COLOR) || this.isRenderAs(morpheus.VectorTrack.RENDER.TEXT_AND_FONT))) {
+      settings.display.push(morpheus.VectorTrack.RENDER.TEXT);
     }
     this._update();
 

vlakam/phantasus.js

src/ui/vector_track.js
@@ -238,11 +238,11 @@ phantasus.VectorTrack.prototype = {
           delete settings.render;
         }
       }
-      if (!this.isRenderAs(phantasus.VectorTrack.RENDER.TEXT) &&
-        (this.isRenderAs(phantasus.VectorTrack.RENDER.TEXT_AND_COLOR) || this.isRenderAs(phantasus.VectorTrack.RENDER.TEXT_AND_FONT))) {
-        settings.display.push(phantasus.VectorTrack.RENDER.TEXT);
-      }
       this.settings = settings;
+    }
+    if (!this.isRenderAs(phantasus.VectorTrack.RENDER.TEXT) &&
+      (this.isRenderAs(phantasus.VectorTrack.RENDER.TEXT_AND_COLOR) || this.isRenderAs(phantasus.VectorTrack.RENDER.TEXT_AND_FONT))) {
+      settings.display.push(phantasus.VectorTrack.RENDER.TEXT);
     }
     this._update();
 