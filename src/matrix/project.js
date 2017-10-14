cmap/morpheus.js

encode text using font and color

src/matrix/project.js
@@ -13,6 +13,8 @@ morpheus.Project = function (dataset) {
   this.columnColorModel = new morpheus.VectorColorModel();
   this.rowShapeModel = new morpheus.VectorShapeModel();
   this.columnShapeModel = new morpheus.VectorShapeModel();
+  this.rowFontModel = new morpheus.VectorFontModel();
+  this.columnFontModel = new morpheus.VectorFontModel();
   this.hoverColumnIndex = -1;
   this.hoverRowIndex = -1;
   this.columnSelectionModel = new morpheus.SelectionModel(this, true);
@@ -21,8 +23,8 @@ morpheus.Project = function (dataset) {
   this.symmetricProjectListener = null;
   morpheus.Project._recomputeCalculatedColumnFields(this.originalDataset, morpheus.VectorKeys.RECOMPUTE_FUNCTION_NEW_HEAT_MAP);
   morpheus.Project
-  ._recomputeCalculatedColumnFields(new morpheus.TransposedDatasetView(
-    this.originalDataset), morpheus.VectorKeys.RECOMPUTE_FUNCTION_NEW_HEAT_MAP);
+    ._recomputeCalculatedColumnFields(new morpheus.TransposedDatasetView(
+      this.originalDataset), morpheus.VectorKeys.RECOMPUTE_FUNCTION_NEW_HEAT_MAP);
 };
 morpheus.Project.Events = {
   DATASET_CHANGED: 'datasetChanged',
@@ -99,6 +101,12 @@ morpheus.Project.prototype = {
   },
   getColumnShapeModel: function () {
     return this.columnShapeModel;
+  },
+  getRowFontModel: function () {
+    return this.rowFontModel;
+  },
+  getColumnFontModel: function () {
+    return this.columnFontModel;
   },
   getGroupRows: function () {
     return this.groupRows;
@@ -185,7 +193,7 @@ morpheus.Project.prototype = {
   getSortedFilteredDataset: function () {
     return morpheus.DatasetUtil.slicedView(this.getFullDataset(),
       this.rowIndexMapper.convertToView(), this.columnIndexMapper
-      .convertToView());
+        .convertToView());
   },
   getSelectedDataset: function (options) {
     options = $.extend({}, {

src/matrix/vector_shape_model.js
@@ -1,9 +1,10 @@
 morpheus.VectorShapeModel = function () {
   this.shapes = morpheus.VectorShapeModel.SHAPES;
-  this.vectorNameToShapeMap = new morpheus.Map();
+  this.vectorNameToMappedValue = new morpheus.Map();
 };
 
-morpheus.VectorShapeModel.SHAPES = ['circle', 'square', 'plus', 'x',
+morpheus.VectorShapeModel.SHAPES = [
+  'circle', 'square', 'plus', 'x',
   'asterisk', 'diamond', 'triangle-up', 'triangle-down', 'triangle-left',
   'triangle-right', 'minus'];
 morpheus.VectorShapeModel.STANDARD_SHAPES = {
@@ -15,22 +16,41 @@ morpheus.VectorShapeModel.STANDARD_SHAPES = {
 };
 
 morpheus.VectorShapeModel.prototype = {
+  toJSON: function (tracks) {
+    var _this = this;
+    var json = {};
+    tracks.forEach(function (track) {
+      if (track.isRenderAs(morpheus.VectorTrack.RENDER.SHAPE)) {
+        var map = _this.vectorNameToMappedValue.get(track.getName());
+        if (map != null) {
+          json[track.getName()] = map;
+        }
+      }
+    });
+    return json;
+  },
+  fromJSON: function (json) {
+    for (var name in json) {
+      var obj = json[name];
+      this.vectorNameToMappedValue.set(name, morpheus.Map.fromJSON(obj));
+    }
+  },
   clear: function (vector) {
-    this.vectorNameToShapeMap.remove(vector.getName());
+    this.vectorNameToMappedValue.remove(vector.getName());
   },
   copy: function () {
     var c = new morpheus.VectorShapeModel();
     c.shapes = this.shapes.slice(0);
-    this.vectorNameToShapeMap.forEach(function (shapeMap, name) {
+    this.vectorNameToMappedValue.forEach(function (shapeMap, name) {
       var newShapeMap = new morpheus.Map();
       newShapeMap.setAll(shapeMap); // copy existing values
-      c.vectorNameToShapeMap.set(name, newShapeMap);
+      c.vectorNameToMappedValue.set(name, newShapeMap);
     });
 
     return c;
   },
   clearAll: function () {
-    this.vectorNameToShapeMap = new morpheus.Map();
+    this.vectorNameToMappedValue = new morpheus.Map();
   },
   _getShapeForValue: function (value) {
     if (value == null) {
@@ -43,8 +63,8 @@ morpheus.VectorShapeModel.prototype = {
     }
 
     // try to reuse existing map
-    var existingMetadataValueToShapeMap = this.vectorNameToShapeMap
-    .values();
+    var existingMetadataValueToShapeMap = this.vectorNameToMappedValue
+      .values();
     for (var i = 0, length = existingMetadataValueToShapeMap.length; i < length; i++) {
       var shape = existingMetadataValueToShapeMap[i].get(value);
       if (shape !== undefined) {
@@ -54,14 +74,14 @@ morpheus.VectorShapeModel.prototype = {
 
   },
   getMap: function (name) {
-    return this.vectorNameToShapeMap.get(name);
+    return this.vectorNameToMappedValue.get(name);
   },
   getMappedValue: function (vector, value) {
-    var metadataValueToShapeMap = this.vectorNameToShapeMap.get(vector
-    .getName());
+    var metadataValueToShapeMap = this.vectorNameToMappedValue.get(vector
+      .getName());
     if (metadataValueToShapeMap === undefined) {
       metadataValueToShapeMap = new morpheus.Map();
-      this.vectorNameToShapeMap.set(vector.getName(),
+      this.vectorNameToMappedValue.set(vector.getName(),
         metadataValueToShapeMap);
       // set all possible shapes
       var values = morpheus.VectorUtil.getValues(vector);
@@ -85,11 +105,11 @@ morpheus.VectorShapeModel.prototype = {
     return shape;
   },
   setMappedValue: function (vector, value, shape) {
-    var metadataValueToShapeMap = this.vectorNameToShapeMap.get(vector
-    .getName());
+    var metadataValueToShapeMap = this.vectorNameToMappedValue.get(vector
+      .getName());
     if (metadataValueToShapeMap === undefined) {
       metadataValueToShapeMap = new morpheus.Map();
-      this.vectorNameToShapeMap.set(vector.getName(),
+      this.vectorNameToMappedValue.set(vector.getName(),
         metadataValueToShapeMap);
     }
     metadataValueToShapeMap.set(value, shape);

src/ui/discrete_color_scheme_chooser.js
@@ -1,22 +1,13 @@
 morpheus.DiscreteColorSchemeChooser = function (options) {
   var formBuilder = new morpheus.FormBuilder();
   var map = options.colorScheme.scale;
-  var html = ['<select name="colorPicker" class="selectpicker" data-live-search="true">'];
-  map.forEach(function (val, key) {
-    html.push('<option');
-    html.push(' value="');
-    html.push(key);
-    html.push('">');
-    html.push(key);
-    html.push('</option>');
-  });
-  html.push('</select>');
+
   formBuilder.append({
     name: 'selected_value',
-    type: 'custom',
-    value: html.join('')
+    type: 'bootstrap-select',
+    options: map.keys()
   });
-  var $select = formBuilder.$form.find('[name=colorPicker]');
+  var $select = formBuilder.find('selected_value');
   formBuilder.append({
     style: 'max-width:50px;',
     name: 'selected_color',
@@ -24,7 +15,7 @@ morpheus.DiscreteColorSchemeChooser = function (options) {
   });
   var selectedVal = $select.val();
   var _this = this;
-  var $color = formBuilder.$form.find('[name=selected_color]');
+  var $color = formBuilder.find('selected_color');
   $color.val(map.get(selectedVal));
   $color.on('change', function (e) {
     var color = $(this).val();
@@ -34,16 +25,12 @@ morpheus.DiscreteColorSchemeChooser = function (options) {
       color: color
     });
   });
-  $select.selectpicker().change(function () {
-    // var optionIndex = sel.prop("selectedIndex");
+  $select.on('change', function () {
     selectedVal = $select.val();
     var c = map.get(selectedVal);
     $color.val(c);
   });
   this.$div = formBuilder.$form;
 };
-morpheus.DiscreteColorSchemeChooser.prototype = {
-  dispose: function () {
-  }
-};
+morpheus.DiscreteColorSchemeChooser.prototype = {};
 morpheus.Util.extend(morpheus.DiscreteColorSchemeChooser, morpheus.Events);

src/ui/font_chooser.js
@@ -1,0 +1,88 @@
+/**
+ *
+ * @param options.fontModel
+ * @param options.track
+ * @param options.heatMap
+ * @constructor
+ */
+morpheus.FontChooser = function (options) {
+  var _this = this;
+  var fontModel = options.fontModel;
+  var track = options.track;
+  var heatMap = options.heatMap;
+  // ensure map exists
+  fontModel.getMappedValue(track.getVector(track.settings.fontField), track.getVector(track.settings.fontField).getValue(0));
+  var formBuilder = new morpheus.FormBuilder();
+  formBuilder.append({
+    value: track.settings.fontField != null,
+    type: 'checkbox',
+    name: 'use_another_annotation_to_determine_font'
+  });
+  var annotationNames = morpheus.MetadataUtil.getMetadataNames(
+    track.isColumns ? heatMap.getProject().getFullDataset().getColumnMetadata() : heatMap.getProject().getFullDataset().getRowMetadata());
+  annotationNames.splice(annotationNames.indexOf(track.getName()), 1);
+  formBuilder.append({
+    name: 'annotation_name',
+    type: 'bootstrap-select',
+    options: annotationNames,
+    search: annotationNames.length > 10,
+    value: track.settings.fontField
+  });
+  formBuilder.setVisible('annotation_name', track.settings.fontField != null);
+  formBuilder.append({
+    name: 'selected_value',
+    type: 'bootstrap-select',
+    search: true,
+    options: fontModel.getMap(track.settings.fontField != null ? track.settings.fontField : track.getName()).keys()
+  });
+  var $selectedValue = formBuilder.find('selected_value');
+  formBuilder.append({
+    name: 'selected_font',
+    type: 'bootstrap-select',
+    options: [{name: 'normal', value: 400}, {name: 'bold', value: 700}, {name: 'bolder', value: 900}]
+  });
+
+  var repaint = function () {
+    track.setInvalid(true);
+    track.repaint();
+  };
+  formBuilder.find('use_another_annotation_to_determine_font').on('change', function () {
+    var checked = $(this).prop('checked');
+    formBuilder.setValue('annotation_name', null);
+    formBuilder.setValue('selected_value', null);
+    formBuilder.setVisible('annotation_name', checked);
+    if (!checked) {
+      track.settings.fontField = null;
+    }
+    repaint();
+  });
+  formBuilder.find('annotation_name').on('change', function () {
+    var annotationName = $(this).val();
+    fontModel.getMappedValue(track.getVector(annotationName), track.getVector(annotationName).getValue(0));
+    track.settings.fontField = annotationName;
+    // ensure map exists
+    formBuilder.setOptions('selected_value', fontModel.getMap(track.settings.fontField != null ? track.settings.fontField : track.getName()).keys());
+    formBuilder.setValue('selected_value', null);
+    repaint();
+  });
+
+  var $selectedFont = formBuilder.find('selected_font');
+  $selectedFont.on('change', function (e) {
+    fontModel.setMappedValue(track.getVector(track.settings.fontField), $selectedValue.val(), {weight: $(this).val()});
+    repaint();
+  });
+
+  var updateMappedValue = function () {
+    var selectedVal = $selectedValue.val();
+    var mappedValue = fontModel.getMappedValue(track.getVector(track.settings.fontField), selectedVal);
+    formBuilder.setValue('selected_font', mappedValue.weight);
+  };
+  $selectedValue.on('change', function () {
+    // update displayed value
+    updateMappedValue();
+  });
+  updateMappedValue();
+  this.$div = formBuilder.$form;
+
+};
+morpheus.Util.extend(morpheus.FontChooser, morpheus.Events);

src/ui/heat_map_track_color_legend.js
@@ -17,12 +17,9 @@ morpheus.HeatMapTrackColorLegend.prototype = {
     for (var i = 0; i < tracks.length; i++) {
       ypix = 0;
       var maxWidth = 0;
-      var vector = tracks[i].getVector();
-      var map = colorModel.getDiscreteColorScheme(vector);
-      if (map == null) { // continuous
-        maxWidth = 220;
-        ypix += 40;
-      } else {
+      var vector = tracks[i].getVector(tracks[i].settings.colorByField);
+      if (vector.getProperties().get(morpheus.VectorKeys.DISCRETE)) {
+        var map = colorModel.getDiscreteColorScheme(vector);
         map.forEach(function (color, key) {
           var width = context.measureText(key).width;
           if (!isNaN(width)) {
@@ -30,6 +27,10 @@ morpheus.HeatMapTrackColorLegend.prototype = {
           }
           ypix += 14;
         });
+      } else {
+        maxWidth = 220;
+        ypix += 40;
+
       }
       maxWidth = Math.max(maxWidth,
         context.measureText(vector.getName()).width);
@@ -48,7 +49,7 @@ morpheus.HeatMapTrackColorLegend.prototype = {
     // legends are placed side by side
     for (var i = 0; i < tracks.length; i++) {
       var ypix = 0;
-      var vector = tracks[i].getVector();
+      var vector = tracks[i].getVector(tracks[i].settings.colorByField);
       context.fillStyle = morpheus.CanvasUtil.FONT_COLOR;
       context.font = '12px ' + morpheus.CanvasUtil.getFontFamily(context);
       context.textAlign = 'left';
@@ -63,17 +64,7 @@ morpheus.HeatMapTrackColorLegend.prototype = {
         maxWidth = Math.max(0, textWidth);
       }
       ypix += 14;
-
-      if (colorModel.isContinuous(vector)) { // draw continuous color legend
-        var scheme = colorModel.getContinuousColorScheme(vector);
-        context.save();
-        context.translate(xpix, ypix);
-        morpheus.HeatMapColorSchemeLegend.drawColorScheme(context,
-          scheme, 200);
-        context.restore();
-        maxWidth = Math.max(maxWidth, 220);
-        ypix += 40;
-      } else {
+      if (vector.getProperties().get(morpheus.VectorKeys.DISCRETE)) {
         var toStringFunction = morpheus.VectorTrack.vectorToString(vector);
         var map = colorModel.getDiscreteColorScheme(vector);
         var values = map.keys().sort(morpheus.SortKey.ASCENDING_COMPARATOR);
@@ -93,6 +84,15 @@ morpheus.HeatMapTrackColorLegend.prototype = {
             ypix += 14;
           }
         });
+      } else {
+        var scheme = colorModel.getContinuousColorScheme(vector);
+        context.save();
+        context.translate(xpix, ypix);
+        morpheus.HeatMapColorSchemeLegend.drawColorScheme(context,
+          scheme, 200);
+        context.restore();
+        maxWidth = Math.max(maxWidth, 220);
+        ypix += 40;
       }
       xpix += maxWidth + 10 + 14; // space between tracks + color chip
     }

vlakam/phantasus.js

src/matrix/project.js
@@ -13,6 +13,8 @@ phantasus.Project = function (dataset) {
   this.columnColorModel = new phantasus.VectorColorModel();
   this.rowShapeModel = new phantasus.VectorShapeModel();
   this.columnShapeModel = new phantasus.VectorShapeModel();
+  this.rowFontModel = new phantasus.VectorFontModel();
+  this.columnFontModel = new phantasus.VectorFontModel();
   this.hoverColumnIndex = -1;
   this.hoverRowIndex = -1;
   this.columnSelectionModel = new phantasus.SelectionModel(this, true);
@@ -21,8 +23,8 @@ phantasus.Project = function (dataset) {
   this.symmetricProjectListener = null;
   phantasus.Project._recomputeCalculatedColumnFields(this.originalDataset, phantasus.VectorKeys.RECOMPUTE_FUNCTION_NEW_HEAT_MAP);
   phantasus.Project
-  ._recomputeCalculatedColumnFields(new phantasus.TransposedDatasetView(
-    this.originalDataset), phantasus.VectorKeys.RECOMPUTE_FUNCTION_NEW_HEAT_MAP);
+    ._recomputeCalculatedColumnFields(new phantasus.TransposedDatasetView(
+      this.originalDataset), phantasus.VectorKeys.RECOMPUTE_FUNCTION_NEW_HEAT_MAP);
 };
 phantasus.Project.Events = {
   DATASET_CHANGED: 'datasetChanged',
@@ -98,6 +100,12 @@ phantasus.Project.prototype = {
   },
   getColumnShapeModel: function () {
     return this.columnShapeModel;
+  },
+  getRowFontModel: function () {
+    return this.rowFontModel;
+  },
+  getColumnFontModel: function () {
+    return this.columnFontModel;
   },
   getGroupRows: function () {
     return this.groupRows;

src/matrix/vector_shape_model.js
@@ -1,9 +1,10 @@
 phantasus.VectorShapeModel = function () {
   this.shapes = phantasus.VectorShapeModel.SHAPES;
-  this.vectorNameToShapeMap = new phantasus.Map();
+  this.vectorNameToMappedValue = new phantasus.Map();
 };
 
-phantasus.VectorShapeModel.SHAPES = ['circle', 'square', 'plus', 'x',
+phantasus.VectorShapeModel.SHAPES = [
+  'circle', 'square', 'plus', 'x',
   'asterisk', 'diamond', 'triangle-up', 'triangle-down', 'triangle-left',
   'triangle-right', 'minus'];
 phantasus.VectorShapeModel.STANDARD_SHAPES = {
@@ -15,22 +16,41 @@ phantasus.VectorShapeModel.STANDARD_SHAPES = {
 };
 
 phantasus.VectorShapeModel.prototype = {
+  toJSON: function (tracks) {
+    var _this = this;
+    var json = {};
+    tracks.forEach(function (track) {
+      if (track.isRenderAs(phantasus.VectorTrack.RENDER.SHAPE)) {
+        var map = _this.vectorNameToMappedValue.get(track.getName());
+        if (map != null) {
+          json[track.getName()] = map;
+        }
+      }
+    });
+    return json;
+  },
+  fromJSON: function (json) {
+    for (var name in json) {
+      var obj = json[name];
+      this.vectorNameToMappedValue.set(name, phantasus.Map.fromJSON(obj));
+    }
+  },
   clear: function (vector) {
-    this.vectorNameToShapeMap.remove(vector.getName());
+    this.vectorNameToMappedValue.remove(vector.getName());
   },
   copy: function () {
     var c = new phantasus.VectorShapeModel();
     c.shapes = this.shapes.slice(0);
-    this.vectorNameToShapeMap.forEach(function (shapeMap, name) {
+    this.vectorNameToMappedValue.forEach(function (shapeMap, name) {
       var newShapeMap = new phantasus.Map();
       newShapeMap.setAll(shapeMap); // copy existing values
-      c.vectorNameToShapeMap.set(name, newShapeMap);
+      c.vectorNameToMappedValue.set(name, newShapeMap);
     });
 
     return c;
   },
   clearAll: function () {
-    this.vectorNameToShapeMap = new phantasus.Map();
+    this.vectorNameToMappedValue = new phantasus.Map();
   },
   _getShapeForValue: function (value) {
     if (value == null) {
@@ -43,7 +63,7 @@ phantasus.VectorShapeModel.prototype = {
     }
 
     // try to reuse existing map
-    var existingMetadataValueToShapeMap = this.vectorNameToShapeMap
+    var existingMetadataValueToShapeMap = this.vectorNameToMappedValue
       .values();
     for (var i = 0, length = existingMetadataValueToShapeMap.length; i < length; i++) {
       var shape = existingMetadataValueToShapeMap[i].get(value);
@@ -54,14 +74,14 @@ phantasus.VectorShapeModel.prototype = {
 
   },
   getMap: function (name) {
-    return this.vectorNameToShapeMap.get(name);
+    return this.vectorNameToMappedValue.get(name);
   },
   getMappedValue: function (vector, value) {
-    var metadataValueToShapeMap = this.vectorNameToShapeMap.get(vector
+    var metadataValueToShapeMap = this.vectorNameToMappedValue.get(vector
       .getName());
     if (metadataValueToShapeMap === undefined) {
       metadataValueToShapeMap = new phantasus.Map();
-      this.vectorNameToShapeMap.set(vector.getName(),
+      this.vectorNameToMappedValue.set(vector.getName(),
         metadataValueToShapeMap);
       // set all possible shapes
       var values = phantasus.VectorUtil.getValues(vector);
@@ -85,11 +105,11 @@ phantasus.VectorShapeModel.prototype = {
     return shape;
   },
   setMappedValue: function (vector, value, shape) {
-    var metadataValueToShapeMap = this.vectorNameToShapeMap.get(vector
+    var metadataValueToShapeMap = this.vectorNameToMappedValue.get(vector
       .getName());
     if (metadataValueToShapeMap === undefined) {
       metadataValueToShapeMap = new phantasus.Map();
-      this.vectorNameToShapeMap.set(vector.getName(),
+      this.vectorNameToMappedValue.set(vector.getName(),
         metadataValueToShapeMap);
     }
     metadataValueToShapeMap.set(value, shape);

src/ui/discrete_color_scheme_chooser.js
@@ -1,22 +1,13 @@
 phantasus.DiscreteColorSchemeChooser = function (options) {
   var formBuilder = new phantasus.FormBuilder();
   var map = options.colorScheme.scale;
-  var html = ['<select name="colorPicker" class="selectpicker" data-live-search="true">'];
-  map.forEach(function (val, key) {
-    html.push('<option');
-    html.push(' value="');
-    html.push(key);
-    html.push('">');
-    html.push(key);
-    html.push('</option>');
-  });
-  html.push('</select>');
+
   formBuilder.append({
     name: 'selected_value',
-    type: 'custom',
-    value: html.join('')
+    type: 'bootstrap-select',
+    options: map.keys()
   });
-  var $select = formBuilder.$form.find('[name=colorPicker]');
+  var $select = formBuilder.find('selected_value');
   formBuilder.append({
     style: 'max-width:50px;',
     name: 'selected_color',
@@ -24,7 +15,7 @@ phantasus.DiscreteColorSchemeChooser = function (options) {
   });
   var selectedVal = $select.val();
   var _this = this;
-  var $color = formBuilder.$form.find('[name=selected_color]');
+  var $color = formBuilder.find('selected_color');
   $color.val(map.get(selectedVal));
   $color.on('change', function (e) {
     var color = $(this).val();
@@ -34,16 +25,12 @@ phantasus.DiscreteColorSchemeChooser = function (options) {
       color: color
     });
   });
-  $select.selectpicker().change(function () {
-    // var optionIndex = sel.prop("selectedIndex");
+  $select.on('change', function () {
     selectedVal = $select.val();
     var c = map.get(selectedVal);
     $color.val(c);
   });
   this.$div = formBuilder.$form;
 };
-phantasus.DiscreteColorSchemeChooser.prototype = {
-  dispose: function () {
-  }
-};
+phantasus.DiscreteColorSchemeChooser.prototype = {};
 phantasus.Util.extend(phantasus.DiscreteColorSchemeChooser, phantasus.Events);

src/ui/font_chooser.js
@@ -1,0 +1,88 @@
+/**
+ *
+ * @param options.fontModel
+ * @param options.track
+ * @param options.heatMap
+ * @constructor
+ */
+phantasus.FontChooser = function (options) {
+  var _this = this;
+  var fontModel = options.fontModel;
+  var track = options.track;
+  var heatMap = options.heatMap;
+  // ensure map exists
+  fontModel.getMappedValue(track.getVector(track.settings.fontField), track.getVector(track.settings.fontField).getValue(0));
+  var formBuilder = new phantasus.FormBuilder();
+  formBuilder.append({
+    value: track.settings.fontField != null,
+    type: 'checkbox',
+    name: 'use_another_annotation_to_determine_font'
+  });
+  var annotationNames = phantasus.MetadataUtil.getMetadataNames(
+    track.isColumns ? heatMap.getProject().getFullDataset().getColumnMetadata() : heatMap.getProject().getFullDataset().getRowMetadata());
+  annotationNames.splice(annotationNames.indexOf(track.getName()), 1);
+  formBuilder.append({
+    name: 'annotation_name',
+    type: 'bootstrap-select',
+    options: annotationNames,
+    search: annotationNames.length > 10,
+    value: track.settings.fontField
+  });
+  formBuilder.setVisible('annotation_name', track.settings.fontField != null);
+  formBuilder.append({
+    name: 'selected_value',
+    type: 'bootstrap-select',
+    search: true,
+    options: fontModel.getMap(track.settings.fontField != null ? track.settings.fontField : track.getName()).keys()
+  });
+  var $selectedValue = formBuilder.find('selected_value');
+  formBuilder.append({
+    name: 'selected_font',
+    type: 'bootstrap-select',
+    options: [{name: 'normal', value: 400}, {name: 'bold', value: 700}, {name: 'bolder', value: 900}]
+  });
+
+  var repaint = function () {
+    track.setInvalid(true);
+    track.repaint();
+  };
+  formBuilder.find('use_another_annotation_to_determine_font').on('change', function () {
+    var checked = $(this).prop('checked');
+    formBuilder.setValue('annotation_name', null);
+    formBuilder.setValue('selected_value', null);
+    formBuilder.setVisible('annotation_name', checked);
+    if (!checked) {
+      track.settings.fontField = null;
+    }
+    repaint();
+  });
+  formBuilder.find('annotation_name').on('change', function () {
+    var annotationName = $(this).val();
+    fontModel.getMappedValue(track.getVector(annotationName), track.getVector(annotationName).getValue(0));
+    track.settings.fontField = annotationName;
+    // ensure map exists
+    formBuilder.setOptions('selected_value', fontModel.getMap(track.settings.fontField != null ? track.settings.fontField : track.getName()).keys());
+    formBuilder.setValue('selected_value', null);
+    repaint();
+  });
+
+  var $selectedFont = formBuilder.find('selected_font');
+  $selectedFont.on('change', function (e) {
+    fontModel.setMappedValue(track.getVector(track.settings.fontField), $selectedValue.val(), {weight: $(this).val()});
+    repaint();
+  });
+
+  var updateMappedValue = function () {
+    var selectedVal = $selectedValue.val();
+    var mappedValue = fontModel.getMappedValue(track.getVector(track.settings.fontField), selectedVal);
+    formBuilder.setValue('selected_font', mappedValue.weight);
+  };
+  $selectedValue.on('change', function () {
+    // update displayed value
+    updateMappedValue();
+  });
+  updateMappedValue();
+  this.$div = formBuilder.$form;
+
+};
+phantasus.Util.extend(phantasus.FontChooser, phantasus.Events);

src/ui/heat_map_track_color_legend.js
@@ -17,12 +17,9 @@ phantasus.HeatMapTrackColorLegend.prototype = {
     for (var i = 0; i < tracks.length; i++) {
       ypix = 0;
       var maxWidth = 0;
-      var vector = tracks[i].getVector();
-      var map = colorModel.getDiscreteColorScheme(vector);
-      if (map == null) { // continuous
-        maxWidth = 220;
-        ypix += 40;
-      } else {
+      var vector = tracks[i].getVector(tracks[i].settings.colorByField);
+      if (vector.getProperties().get(phantasus.VectorKeys.DISCRETE)) {
+        var map = colorModel.getDiscreteColorScheme(vector);
         map.forEach(function (color, key) {
           var width = context.measureText(key).width;
           if (!isNaN(width)) {
@@ -30,6 +27,10 @@ phantasus.HeatMapTrackColorLegend.prototype = {
           }
           ypix += 14;
         });
+      } else {
+        maxWidth = 220;
+        ypix += 40;
+
       }
       maxWidth = Math.max(maxWidth,
         context.measureText(vector.getName()).width);
@@ -48,7 +49,7 @@ phantasus.HeatMapTrackColorLegend.prototype = {
     // legends are placed side by side
     for (var i = 0; i < tracks.length; i++) {
       var ypix = 0;
-      var vector = tracks[i].getVector();
+      var vector = tracks[i].getVector(tracks[i].settings.colorByField);
       context.fillStyle = phantasus.CanvasUtil.FONT_COLOR;
       context.font = '12px ' + phantasus.CanvasUtil.getFontFamily(context);
       context.textAlign = 'left';
@@ -63,17 +64,7 @@ phantasus.HeatMapTrackColorLegend.prototype = {
         maxWidth = Math.max(0, textWidth);
       }
       ypix += 14;
-
-      if (colorModel.isContinuous(vector)) { // draw continuous color legend
-        var scheme = colorModel.getContinuousColorScheme(vector);
-        context.save();
-        context.translate(xpix, ypix);
-        phantasus.HeatMapColorSchemeLegend.drawColorScheme(context,
-          scheme, 200);
-        context.restore();
-        maxWidth = Math.max(maxWidth, 220);
-        ypix += 40;
-      } else {
+      if (vector.getProperties().get(phantasus.VectorKeys.DISCRETE)) {
         var toStringFunction = phantasus.VectorTrack.vectorToString(vector);
         var map = colorModel.getDiscreteColorScheme(vector);
         var values = map.keys().sort(phantasus.SortKey.ASCENDING_COMPARATOR);
@@ -93,6 +84,15 @@ phantasus.HeatMapTrackColorLegend.prototype = {
             ypix += 14;
           }
         });
+      } else {
+        var scheme = colorModel.getContinuousColorScheme(vector);
+        context.save();
+        context.translate(xpix, ypix);
+        phantasus.HeatMapColorSchemeLegend.drawColorScheme(context,
+          scheme, 200);
+        context.restore();
+        maxWidth = Math.max(maxWidth, 220);
+        ypix += 40;
       }
       xpix += maxWidth + 10 + 14; // space between tracks + color chip
     }