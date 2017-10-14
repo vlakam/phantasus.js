cmap/morpheus.js

improved bubble heat map

src/matrix/vector_shape_model.js
@@ -6,14 +6,11 @@ morpheus.VectorShapeModel = function () {
 morpheus.VectorShapeModel.SHAPES = [
   'circle', 'square', 'plus', 'x',
   'asterisk', 'diamond', 'triangle-up', 'triangle-down', 'triangle-left',
-  'triangle-right', 'minus'];
-morpheus.VectorShapeModel.STANDARD_SHAPES = {
-  cp: 'diamond',
-  oe: 'plus',
-  pcl: 'asterisk',
-  kd: 'minus',
-  ctrl: 'circle'
-};
+  'triangle-right', 'circle-minus'];
+
+morpheus.VectorShapeModel.FILLED_SHAPES = [
+  'circle', 'square', 'diamond', 'triangle-up', 'triangle-down', 'triangle-left',
+  'triangle-right'];
 
 morpheus.VectorShapeModel.prototype = {
   toJSON: function (tracks) {
@@ -55,11 +52,6 @@ morpheus.VectorShapeModel.prototype = {
   _getShapeForValue: function (value) {
     if (value == null) {
       return 'none';
-    }
-    var str = value.toString().toLowerCase();
-    var mapped = morpheus.VectorShapeModel.STANDARD_SHAPES[str];
-    if (mapped !== undefined) {
-      return mapped;
     }
 
     // try to reuse existing map

src/ui/canvas_util.js
@@ -51,12 +51,12 @@ morpheus.CanvasUtil.setBounds = function (canvas, bounds) {
   }
 };
 
-morpheus.CanvasUtil.drawShape = function (context, shape, x, y, size2) {
+morpheus.CanvasUtil.drawShape = function (context, shape, x, y, size2, isFill) {
   if (size2 < 0) {
     return;
   }
   context.beginPath();
-  if (shape === 'minus') {
+  if (shape === 'circle-minus') {
     context.arc(x, y, size2, 0, 2 * Math.PI, false);
     context.moveTo(x - size2, y);
     context.lineTo(x + size2, y);
@@ -130,7 +130,7 @@ morpheus.CanvasUtil.drawShape = function (context, shape, x, y, size2) {
     context.lineTo(x - size2, y - size2);
     context.lineTo(x + size2, y);
   }
-  context.stroke();
+  isFill ? context.fill() : context.stroke();
 
 };
 morpheus.CanvasUtil.drawLine = function (context, x1, y1, x2, y2) {

src/ui/shape_chooser.js
@@ -17,7 +17,7 @@ morpheus.ShapeChooser = function (options) {
     value: html.join('')
   });
 
-  var shapeField = new morpheus.ShapeField();
+  var shapeField = new morpheus.ShapeField({showNone: true});
 
   formBuilder.append({
     style: 'max-width:50px;',

src/ui/shape_field.js
@@ -1,15 +1,26 @@
-morpheus.ShapeField = function (shapes) {
-  shapes = shapes || morpheus.VectorShapeModel.SHAPES;
+/**
+ *
+ * @param options.shapes Array of shape names or null to use morpheus.VectorShapeModel.SHAPES
+ * @param options.showNone Whether none should be an option
+ * @constructor
+ */
+morpheus.ShapeField = function (options) {
+  var shapes = options.shapes || morpheus.VectorShapeModel.SHAPES;
+
   var _this = this;
   var html = [];
   var size2 = 8;
   var x = 4;
   var y = 4;
   html
-  .push('<div style="margin-bottom:1em;" class="btn-group">');
+    .push('<div style="margin-bottom:1em;" class="btn-group">');
   html
-  .push('<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span data-name="selection"></span> <span class="fa fa-caret-down"></span></button>');
+    .push(
+      '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span data-name="selection"></span> <span class="fa fa-caret-down"></span></button>');
   html.push('<ul class="dropdown-menu" role="menu">');
+  if (options.showNone) {
+    html.push('<li><a data-name="none" href="#">(None)</a></li>');
+  }
   for (var i = 0; i < shapes.length; i++) {
     var context = new C2S(size2 * 2, size2 * 2);
     context.translate(4, 4);
@@ -18,11 +29,12 @@ morpheus.ShapeField = function (shapes) {
     html.push('<li><a data-name="' + shapes[i] + '" href="#">' + svg
       + '</a></li>');
   }
-  html.push('<li><a data-name="none" href="#">(None)</a></li>');
+
   html.push('</ul></div>');
   var $el = $(html.join(''));
   var $header = $el.find('[data-name=selection]');
   $el.on('click', 'li > a', function (e) {
+    e.preventDefault();
     var shape = $(this).data('name');
     setShapeValue(shape);
     _this.trigger('change', {

vlakam/phantasus.js

src/matrix/vector_shape_model.js
@@ -6,14 +6,11 @@ phantasus.VectorShapeModel = function () {
 phantasus.VectorShapeModel.SHAPES = [
   'circle', 'square', 'plus', 'x',
   'asterisk', 'diamond', 'triangle-up', 'triangle-down', 'triangle-left',
-  'triangle-right', 'minus'];
-phantasus.VectorShapeModel.STANDARD_SHAPES = {
-  cp: 'diamond',
-  oe: 'plus',
-  pcl: 'asterisk',
-  kd: 'minus',
-  ctrl: 'circle'
-};
+  'triangle-right', 'circle-minus'];
+
+phantasus.VectorShapeModel.FILLED_SHAPES = [
+  'circle', 'square', 'diamond', 'triangle-up', 'triangle-down', 'triangle-left',
+  'triangle-right'];
 
 phantasus.VectorShapeModel.prototype = {
   toJSON: function (tracks) {
@@ -55,11 +52,6 @@ phantasus.VectorShapeModel.prototype = {
   _getShapeForValue: function (value) {
     if (value == null) {
       return 'none';
-    }
-    var str = value.toString().toLowerCase();
-    var mapped = phantasus.VectorShapeModel.STANDARD_SHAPES[str];
-    if (mapped !== undefined) {
-      return mapped;
     }
 
     // try to reuse existing map

src/ui/canvas_util.js
@@ -51,12 +51,12 @@ phantasus.CanvasUtil.setBounds = function (canvas, bounds) {
   }
 };
 
-phantasus.CanvasUtil.drawShape = function (context, shape, x, y, size2) {
+phantasus.CanvasUtil.drawShape = function (context, shape, x, y, size2, isFill) {
   if (size2 < 0) {
     return;
   }
   context.beginPath();
-  if (shape === 'minus') {
+  if (shape === 'circle-minus') {
     context.arc(x, y, size2, 0, 2 * Math.PI, false);
     context.moveTo(x - size2, y);
     context.lineTo(x + size2, y);
@@ -130,7 +130,7 @@ phantasus.CanvasUtil.drawShape = function (context, shape, x, y, size2) {
     context.lineTo(x - size2, y - size2);
     context.lineTo(x + size2, y);
   }
-  context.stroke();
+  isFill ? context.fill() : context.stroke();
 
 };
 phantasus.CanvasUtil.drawLine = function (context, x1, y1, x2, y2) {

src/ui/shape_chooser.js
@@ -17,7 +17,7 @@ phantasus.ShapeChooser = function (options) {
     value: html.join('')
   });
 
-  var shapeField = new phantasus.ShapeField();
+  var shapeField = new phantasus.ShapeField({showNone: true});
 
   formBuilder.append({
     style: 'max-width:50px;',

src/ui/shape_field.js
@@ -1,5 +1,12 @@
-phantasus.ShapeField = function (shapes) {
-  shapes = shapes || phantasus.VectorShapeModel.SHAPES;
+/**
+ *
+ * @param options.shapes Array of shape names or null to use phantasus.VectorShapeModel.SHAPES
+ * @param options.showNone Whether none should be an option
+ * @constructor
+ */
+phantasus.ShapeField = function (options) {
+  var shapes = options.shapes || phantasus.VectorShapeModel.SHAPES;
+
   var _this = this;
   var html = [];
   var size2 = 8;
@@ -8,8 +15,12 @@ phantasus.ShapeField = function (shapes) {
   html
     .push('<div style="margin-bottom:1em;" class="btn-group">');
   html
-    .push('<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span data-name="selection"></span> <span class="fa fa-caret-down"></span></button>');
+    .push(
+      '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span data-name="selection"></span> <span class="fa fa-caret-down"></span></button>');
   html.push('<ul class="dropdown-menu" role="menu">');
+  if (options.showNone) {
+    html.push('<li><a data-name="none" href="#">(None)</a></li>');
+  }
   for (var i = 0; i < shapes.length; i++) {
     var context = new C2S(size2 * 2, size2 * 2);
     context.translate(4, 4);
@@ -18,11 +29,12 @@ phantasus.ShapeField = function (shapes) {
     html.push('<li><a data-name="' + shapes[i] + '" href="#">' + svg
       + '</a></li>');
   }
-  html.push('<li><a data-name="none" href="#">(None)</a></li>');
+
   html.push('</ul></div>');
   var $el = $(html.join(''));
   var $header = $el.find('[data-name=selection]');
   $el.on('click', 'li > a', function (e) {
+    e.preventDefault();
     var shape = $(this).data('name');
     setShapeValue(shape);
     _this.trigger('change', {