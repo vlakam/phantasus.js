cmap/morpheus.js

k vector number format

src/tools/marker_selection.js
@@ -5,7 +5,8 @@ morpheus.MarkerSelection = function () {
 /**
  * @private
  */
-morpheus.MarkerSelection.Functions = [morpheus.FisherExact,
+morpheus.MarkerSelection.Functions = [
+  morpheus.FisherExact,
   morpheus.FoldChange, morpheus.MeanDifference, morpheus.SignalToNoise,
   morpheus.createSignalToNoiseAdjust(), morpheus.TTest];
 
@@ -47,9 +48,9 @@ morpheus.MarkerSelection.prototype = {
       var ids = [];
       if (fieldNames != null) {
         var vectors = morpheus.MetadataUtil.getVectors(project
-        .getFullDataset().getColumnMetadata(), fieldNames);
+          .getFullDataset().getColumnMetadata(), fieldNames);
         var idToIndices = morpheus.VectorUtil
-        .createValuesToIndicesMap(vectors);
+          .createValuesToIndicesMap(vectors);
         idToIndices.forEach(function (indices, id) {
           ids.push(id);
         });
@@ -82,7 +83,7 @@ morpheus.MarkerSelection.prototype = {
   gui: function (project) {
     var dataset = project.getSortedFilteredDataset();
     var fields = morpheus.MetadataUtil.getMetadataNames(dataset
-    .getColumnMetadata());
+      .getColumnMetadata());
     return [
       {
         name: 'metric',
@@ -94,7 +95,7 @@ morpheus.MarkerSelection.prototype = {
       {
         name: 'grouping_value',
         value: '1',
-        help: 'Class values are categorized into two groups based on whether dataset values are greater than or equal to this value',
+        help: 'Class values are categorized into two groups based on whether dataset values are greater than or equal to this value'
       },
       {
         name: 'field',
@@ -170,7 +171,7 @@ morpheus.MarkerSelection.prototype = {
     }
 
     var vectors = morpheus.MetadataUtil.getVectors(dataset
-    .getColumnMetadata(), fieldNames);
+      .getColumnMetadata(), fieldNames);
     var idToIndices = morpheus.VectorUtil.createValuesToIndicesMap(vectors);
     var aIndices = [];
     var bIndices = [];
@@ -190,7 +191,7 @@ morpheus.MarkerSelection.prototype = {
     });
 
     var f = morpheus.MarkerSelection.Functions
-    .fromString(options.input.metric);
+      .fromString(options.input.metric);
 
     var classASet = {};
     for (var i = 0; i < aIndices.length; i++) {
@@ -237,6 +238,7 @@ morpheus.MarkerSelection.prototype = {
     for (var i = 0; i < bIndices.length; i++) {
       comparisonVector.setValue(bIndices[i], 'B');
     }
+
     function done(result) {
       if (result) {
         var pvalueVector = dataset.getRowMetadata().add('p_value');
@@ -248,6 +250,7 @@ morpheus.MarkerSelection.prototype = {
           kVector.setValue(i, result.k[i]);
           v.setValue(i, result.scores[i]);
         }
+        kVector.getProperties().set(morpheus.VectorKeys.FORMATTER, {pattern: 'i'});
         vectors.push(pvalueVector);
         vectors.push(fdrVector);
         vectors.push(kVector);
@@ -268,13 +271,14 @@ morpheus.MarkerSelection.prototype = {
           new morpheus.TopNFilter(
             parseInt(options.input.number_of_markers),
             morpheus.TopNFilter.TOP_BOTTOM, vectors[0]
-            .getName()), true);
+              .getName()), true);
       }
 
       project.setRowFilter(project.getRowFilter(), true);
-      project.setRowSortKeys([new morpheus.SortKey(vectors[0].getName(),
-        isFishy ? morpheus.SortKey.SortOrder.ASCENDING
-          : morpheus.SortKey.SortOrder.DESCENDING)], true);
+      project.setRowSortKeys([
+        new morpheus.SortKey(vectors[0].getName(),
+          isFishy ? morpheus.SortKey.SortOrder.ASCENDING
+            : morpheus.SortKey.SortOrder.DESCENDING)], true);
       // select samples used in comparison
       var selectedColumnIndices = new morpheus.Set();
       aIndices.forEach(function (index) {
@@ -285,8 +289,9 @@ morpheus.MarkerSelection.prototype = {
       });
       project.getColumnSelectionModel().setViewIndices(selectedColumnIndices, true);
 
-      project.setColumnSortKeys([new morpheus.SortKey(comparisonVector
-      .getName(), morpheus.SortKey.SortOrder.ASCENDING)], true);
+      project.setColumnSortKeys([
+        new morpheus.SortKey(comparisonVector
+          .getName(), morpheus.SortKey.SortOrder.ASCENDING)], true);
 
       project.trigger('trackChanged', {
         vectors: vectors,
@@ -341,10 +346,11 @@ morpheus.MarkerSelection.prototype = {
         options.input.npermutations = npermutations;
         if (options.input.background) {
           var blob = new Blob(
-            ['self.onmessage = function(e) {'
-            + 'importScripts(e.data.scripts);'
-            + 'self.postMessage(morpheus.MarkerSelection.execute(morpheus.Dataset.fromJSON(e.data.dataset), e.data.input));'
-            + '}']);
+            [
+              'self.onmessage = function(e) {'
+              + 'importScripts(e.data.scripts);'
+              + 'self.postMessage(morpheus.MarkerSelection.execute(morpheus.Dataset.fromJSON(e.data.dataset), e.data.input));'
+              + '}']);
 
           var url = window.URL.createObjectURL(blob);
           var worker = new Worker(url);

src/tools/nearest_neighbors.js
@@ -21,7 +21,7 @@ morpheus.NearestNeighbors.execute = function (dataset, input) {
     rowSpecificPValues: permutations.rowSpecificPValues,
     k: permutations.k,
     fdr: permutations.fdr,
-    scores: permutations.scores,
+    scores: permutations.scores
   };
 };
 morpheus.NearestNeighbors.prototype = {
@@ -73,22 +73,22 @@ morpheus.NearestNeighbors.prototype = {
         name: 'metric',
         options: morpheus.NearestNeighbors.Functions,
         value: morpheus.Pearson.toString(),
-        type: 'select',
+        type: 'select'
       }, {
         name: 'compute_nearest_neighbors_of',
         options: ['selected rows', 'selected columns', 'column annotation', 'row annotation'],
         value: 'selected rows',
-        type: 'radio',
+        type: 'radio'
       }, {
         name: 'use_selected_only',
-        type: 'checkbox',
+        type: 'checkbox'
       }, {
         name: 'annotation',
-        type: 'bootstrap-select',
+        type: 'bootstrap-select'
       }, {
         name: 'permutations',
         value: '0',
-        type: 'text',
+        type: 'text'
       }];
   },
   execute: function (options) {
@@ -132,7 +132,7 @@ morpheus.NearestNeighbors.prototype = {
         var newDataset = new morpheus.Dataset({
           name: '',
           rows: 1,
-          columns: d1.getColumnCount(),
+          columns: d1.getColumnCount()
         });
         for (var j = 0, ncols = d1.getColumnCount(); j < ncols; j++) {
           var v = morpheus.Percentile(columnView.setIndex(j), 50);
@@ -164,11 +164,12 @@ morpheus.NearestNeighbors.prototype = {
           kVector.setValue(i, result.k[i]);
           scoreVector.setValue(i, result.scores[i]);
         }
+        kVector.getProperties().set(morpheus.VectorKeys.FORMATTER, {pattern: 'i'});
         var vectors = [pvalueVector, fdrVector, kVector, scoreVector];
         project.trigger('trackChanged', {
           vectors: vectors,
           display: ['text'],
-          columns: isColumns,
+          columns: isColumns
         });
       };
 
@@ -194,9 +195,9 @@ morpheus.NearestNeighbors.prototype = {
           dataset: morpheus.Dataset.toJSON(dataset, {
             columnFields: [],
             rowFields: [],
-            seriesIndices: [0],
+            seriesIndices: [0]
           }),
-          input: options.input,
+          input: options.input
         });
 
         worker.onmessage = function (e) {
@@ -227,9 +228,9 @@ morpheus.NearestNeighbors.prototype = {
       project.trigger('trackChanged', {
         vectors: [scoreVector],
         display: ['text'],
-        columns: isColumns,
+        columns: isColumns
       });
     }
 
-  },
+  }
 };

vlakam/phantasus.js

src/tools/marker_selection.js
@@ -5,7 +5,8 @@ phantasus.MarkerSelection = function () {
 /**
  * @private
  */
-phantasus.MarkerSelection.Functions = [phantasus.FisherExact,
+phantasus.MarkerSelection.Functions = [
+  phantasus.FisherExact,
   phantasus.FoldChange, phantasus.MeanDifference, phantasus.SignalToNoise,
   phantasus.createSignalToNoiseAdjust(), phantasus.TTest];
 
@@ -94,7 +95,7 @@ phantasus.MarkerSelection.prototype = {
       {
         name: 'grouping_value',
         value: '1',
-        help: 'Class values are categorized into two groups based on whether dataset values are greater than or equal to this value',
+        help: 'Class values are categorized into two groups based on whether dataset values are greater than or equal to this value'
       },
       {
         name: 'field',
@@ -170,7 +171,7 @@ phantasus.MarkerSelection.prototype = {
     }
 
     var vectors = phantasus.MetadataUtil.getVectors(dataset
-    .getColumnMetadata(), fieldNames);
+      .getColumnMetadata(), fieldNames);
     var idToIndices = phantasus.VectorUtil.createValuesToIndicesMap(vectors);
     var aIndices = [];
     var bIndices = [];
@@ -190,7 +191,7 @@ phantasus.MarkerSelection.prototype = {
     });
 
     var f = phantasus.MarkerSelection.Functions
-    .fromString(options.input.metric);
+      .fromString(options.input.metric);
 
     var classASet = {};
     for (var i = 0; i < aIndices.length; i++) {
@@ -237,6 +238,7 @@ phantasus.MarkerSelection.prototype = {
     for (var i = 0; i < bIndices.length; i++) {
       comparisonVector.setValue(bIndices[i], 'B');
     }
+
     function done(result) {
       if (result) {
         var pvalueVector = dataset.getRowMetadata().add('p_value');
@@ -248,6 +250,7 @@ phantasus.MarkerSelection.prototype = {
           kVector.setValue(i, result.k[i]);
           v.setValue(i, result.scores[i]);
         }
+        kVector.getProperties().set(phantasus.VectorKeys.FORMATTER, {pattern: 'i'});
         vectors.push(pvalueVector);
         vectors.push(fdrVector);
         vectors.push(kVector);
@@ -272,9 +275,10 @@ phantasus.MarkerSelection.prototype = {
       }
 
       project.setRowFilter(project.getRowFilter(), true);
-      project.setRowSortKeys([new phantasus.SortKey(vectors[0].getName(),
-        isFishy ? phantasus.SortKey.SortOrder.ASCENDING
-          : phantasus.SortKey.SortOrder.DESCENDING)], true);
+      project.setRowSortKeys([
+        new phantasus.SortKey(vectors[0].getName(),
+          isFishy ? phantasus.SortKey.SortOrder.ASCENDING
+            : phantasus.SortKey.SortOrder.DESCENDING)], true);
       // select samples used in comparison
       var selectedColumnIndices = new phantasus.Set();
       aIndices.forEach(function (index) {
@@ -285,8 +289,9 @@ phantasus.MarkerSelection.prototype = {
       });
       project.getColumnSelectionModel().setViewIndices(selectedColumnIndices, true);
 
-      project.setColumnSortKeys([new phantasus.SortKey(comparisonVector
-        .getName(), phantasus.SortKey.SortOrder.ASCENDING)], true);
+      project.setColumnSortKeys([
+        new phantasus.SortKey(comparisonVector
+          .getName(), phantasus.SortKey.SortOrder.ASCENDING)], true);
 
       project.trigger('trackChanged', {
         vectors: vectors,
@@ -341,10 +346,11 @@ phantasus.MarkerSelection.prototype = {
         options.input.npermutations = npermutations;
         if (options.input.background) {
           var blob = new Blob(
-            ['self.onmessage = function(e) {'
-            + 'importScripts(e.data.scripts);'
-            + 'self.postMessage(phantasus.MarkerSelection.execute(phantasus.Dataset.fromJSON(e.data.dataset), e.data.input));'
-            + '}']);
+            [
+              'self.onmessage = function(e) {'
+              + 'importScripts(e.data.scripts);'
+              + 'self.postMessage(phantasus.MarkerSelection.execute(phantasus.Dataset.fromJSON(e.data.dataset), e.data.input));'
+              + '}']);
 
           var url = window.URL.createObjectURL(blob);
           var worker = new Worker(url);

src/tools/nearest_neighbors.js
@@ -21,7 +21,7 @@ phantasus.NearestNeighbors.execute = function (dataset, input) {
     rowSpecificPValues: permutations.rowSpecificPValues,
     k: permutations.k,
     fdr: permutations.fdr,
-    scores: permutations.scores,
+    scores: permutations.scores
   };
 };
 phantasus.NearestNeighbors.prototype = {
@@ -70,26 +70,26 @@ phantasus.NearestNeighbors.prototype = {
   gui: function () {
     return [
       {
-      name: 'metric',
-      options: phantasus.NearestNeighbors.Functions,
-      value: phantasus.Pearson.toString(),
-      type: 'select',
-    }, {
-      name: 'compute_nearest_neighbors_of',
-      options: ['selected rows', 'selected columns', 'column annotation', 'row annotation'],
-      value: 'selected rows',
-      type: 'radio',
-    }, {
-      name: 'use_selected_only',
-      type: 'checkbox',
-    }, {
-      name: 'annotation',
-      type: 'bootstrap-select',
-    }, {
-      name: 'permutations',
-      value: '0',
-      type: 'text',
-    }];
+        name: 'metric',
+        options: phantasus.NearestNeighbors.Functions,
+        value: phantasus.Pearson.toString(),
+        type: 'select'
+      }, {
+        name: 'compute_nearest_neighbors_of',
+        options: ['selected rows', 'selected columns', 'column annotation', 'row annotation'],
+        value: 'selected rows',
+        type: 'radio'
+      }, {
+        name: 'use_selected_only',
+        type: 'checkbox'
+      }, {
+        name: 'annotation',
+        type: 'bootstrap-select'
+      }, {
+        name: 'permutations',
+        value: '0',
+        type: 'text'
+      }];
   },
   execute: function (options) {
     var project = options.project;
@@ -132,7 +132,7 @@ phantasus.NearestNeighbors.prototype = {
         var newDataset = new phantasus.Dataset({
           name: '',
           rows: 1,
-          columns: d1.getColumnCount(),
+          columns: d1.getColumnCount()
         });
         for (var j = 0, ncols = d1.getColumnCount(); j < ncols; j++) {
           var v = phantasus.Percentile(columnView.setIndex(j), 50);
@@ -164,11 +164,12 @@ phantasus.NearestNeighbors.prototype = {
           kVector.setValue(i, result.k[i]);
           scoreVector.setValue(i, result.scores[i]);
         }
+        kVector.getProperties().set(phantasus.VectorKeys.FORMATTER, {pattern: 'i'});
         var vectors = [pvalueVector, fdrVector, kVector, scoreVector];
         project.trigger('trackChanged', {
           vectors: vectors,
           display: ['text'],
-          columns: isColumns,
+          columns: isColumns
         });
       };
 
@@ -194,9 +195,9 @@ phantasus.NearestNeighbors.prototype = {
           dataset: phantasus.Dataset.toJSON(dataset, {
             columnFields: [],
             rowFields: [],
-            seriesIndices: [0],
+            seriesIndices: [0]
           }),
-          input: options.input,
+          input: options.input
         });
 
         worker.onmessage = function (e) {
@@ -227,9 +228,9 @@ phantasus.NearestNeighbors.prototype = {
       project.trigger('trackChanged', {
         vectors: [scoreVector],
         display: ['text'],
-        columns: isColumns,
+        columns: isColumns
       });
     }
 
-  },
+  }
 };