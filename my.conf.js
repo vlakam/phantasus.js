aeb5e198293989a6de7a03175729f1d9050f58ef
baba-beda <darya.nightmare@gmail.com>
2017-10-15 14:25:56 +0300

    adjust_tool_test now is passed

--- my.conf.js
+++ my.conf.js
@@ -42,7 +42,6 @@ module.exports = function(config) {
     exclude: ['jasmine/spec/marker_selection_test.js',
               'jasmine/spec/nearest_neighbors_test.js',
               'jasmine/spec/qnorm_test.js',
-              'jasmine/spec/adjust_tool_test.js',
               'jasmine/spec/save_dataset_test.js'],
 
 
