929d64d357764fcafc66f7bd3c93d73215275974
baba-beda <darya.nightmare@gmail.com>
2017-10-17 17:58:20 +0300

    a small test for selectRow

--- jasmine/spec/collapse_tool_test.js
+++ jasmine/spec/collapse_tool_test.js
@@ -1,4 +1,56 @@
 describe('collapse_tool_test', function () {
+  it('maxmeanprobe',
+    function () {
+      var heatmap = new phantasus.HeatMap({
+        dataset: new phantasus.Dataset({
+          array: [[1, 3], [4, 6], [7, 9], [10, 12]],
+          rows: 4,
+          columns: 2
+        })
+      });
+      heatmap.getProject().getFullDataset().getRowMetadata()
+        .add('id').array = ['a', 'b', 'a', 'a'];
+      var newHeatMap = new phantasus.CollapseDatasetTool().execute({
+        heatMap: heatmap,
+        project: heatmap.getProject(),
+        input: {
+          collapse_method: 'Maximum Mean Probe',
+          collapse_to_fields: ['id']
+        }
+      });
+      expect(newHeatMap.getProject().getFullDataset())
+        .toBeDatasetValues(new phantasus.Dataset({
+          array: [[4, 6], [10, 12]],
+          rows: 2,
+          columns: 2
+        }), 0.00001);
+    });
+  it('maxmedianprobe',
+    function () {
+      var heatmap = new phantasus.HeatMap({
+        dataset: new phantasus.Dataset({
+          array: [[1, 3], [4, 6], [7, 9], [10, 12]],
+          rows: 4,
+          columns: 2
+        })
+      });
+      heatmap.getProject().getFullDataset().getRowMetadata()
+        .add('id').array = ['a', 'b', 'a', 'a'];
+      var newHeatMap = new phantasus.CollapseDatasetTool().execute({
+        heatMap: heatmap,
+        project: heatmap.getProject(),
+        input: {
+          collapse_method: 'Maximum Median Probe',
+          collapse_to_fields: ['id']
+        }
+      });
+      expect(newHeatMap.getProject().getFullDataset())
+        .toBeDatasetValues(new phantasus.Dataset({
+          array: [[4, 6], [10, 12]],
+          rows: 2,
+          columns: 2
+        }), 0.00001);
+    });
   it('mean',
     function () {
       var heatmap = new phantasus.HeatMap({
